8e41d394184589b9dd1e162ff2b6483e9c9cc8b0
Vyatcheslav Suharnikov <arz.freezy@gmail.com>
2019-07-07 09:57:51 +0300

    DEX-344 A documentation generator for DEX errors
    Fixed the "Adapting argument list by creating a N-tuple" issue

--- dex/src/main/scala/com/wavesplatform/dex/error/Implicits.scala
+++ dex/src/main/scala/com/wavesplatform/dex/error/Implicits.scala
@@ -1,47 +1,38 @@
 package com.wavesplatform.dex.error
 
 import cats.Show
-import play.api.libs.json.{JsObject, JsValue, Json, Writes}
+import play.api.libs.json.{JsObject, JsValue, Writes}
 import shapeless.ops.hlist.{Mapper, ToList}
-import shapeless.{Generic, HList, Id, Poly1}
+import shapeless.{HList, Id, Poly1, ProductArgs}
 
 object Implicits {
   implicit class ErrorInterpolator(sc: StringContext) {
-    def e(): MatcherErrorMessage = {
-      val x = sc.parts.head.trim
-      MatcherErrorMessage(x, x, JsObject.empty)
-    }
+    class Args extends ProductArgs {
+      def applyProduct[H <: HList, L <: HList](args: H)(
+          implicit
+          formatArgs: Mapper.Aux[FormatArg.type, H, L],
+          toList: ToList[L, Id[_]]
+      ): MatcherErrorMessage = {
+        val parts = sc.parts.init
 
-    def e[T](arg: (Symbol, T))(implicit show: Show[T], json: Writes[T]): MatcherErrorMessage = {
-      val name = arg._1.name
-      MatcherErrorMessage(
-        normalize(s"${sc.parts.head}$name${sc.parts.last}"),
-        normalize(s"${sc.parts.head}${show.show(arg._2)}${sc.parts.last}"),
-        Json.obj(name -> json.writes(arg._2))
-      )
-    }
+        val (nameArgs, strArgs, jsonArgs) = toList(formatArgs(args)).asInstanceOf[List[(String, String, JsValue)]].unzip3
+        val (message, template, params) = parts.zipWithIndex.foldLeft(("", "", JsObject.empty)) {
+          case ((m, t, p), (x, i)) =>
+            val name = nameArgs(i)
+            val str  = strArgs(i)
+            val json = jsonArgs(i)
+            (s"$m$x$str", s"$t$x{{$name}}", p + (name -> json))
+        }
 
-    def e[T, H <: HList, L <: HList](args: T)(implicit
-                                              gen: Generic.Aux[T, H],
-                                              formatArgs: Mapper.Aux[FormatArg.type, H, L],
-                                              toList: ToList[L, Id[_]]): MatcherErrorMessage = {
-      val parts = sc.parts.init
-
-      val (nameArgs, strArgs, jsonArgs) = toList(formatArgs(gen.to(args))).asInstanceOf[List[(String, String, JsValue)]].unzip3
-      val (message, template, params) = parts.zipWithIndex.foldLeft(("", "", JsObject.empty)) {
-        case ((m, t, p), (x, i)) =>
-          val name = nameArgs(i)
-          val str  = strArgs(i)
-          val json = jsonArgs(i)
-          (s"$m$x$str", s"$t$x{{$name}}", p + (name -> json))
+        MatcherErrorMessage(
+          normalize(message + sc.parts.last),
+          normalize(template + sc.parts.last),
+          params
+        )
       }
-
-      MatcherErrorMessage(
-        normalize(message + sc.parts.last),
-        normalize(template + sc.parts.last),
-        params
-      )
     }
+
+    val e: Args = new Args
 
     def normalize(x: String): String = x.stripMargin('|').replaceAll("\n", " ").trim
   }

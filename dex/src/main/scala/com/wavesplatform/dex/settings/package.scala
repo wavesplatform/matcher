be2183b0117efe040d4b7ccf158bfa3c47a3ad6d
Denis Serebryansky <denis.serebryansky.eu@gmail.com>
2019-10-29 16:51:30 +0300

    DEX-409 Tick size now can be correctly represented for the pairs with 16 decimals

--- dex/src/main/scala/com/wavesplatform/dex/settings/package.scala
+++ dex/src/main/scala/com/wavesplatform/dex/settings/package.scala
@@ -2,7 +2,7 @@ package com.wavesplatform.dex
 
 package object settings {
 
-  private val format = new java.text.DecimalFormat("#.##############")
+  private val format = new java.text.DecimalFormat("#.################")
 
   /** Formats amount or price */
   def formatValue(value: Double): String = format.format(value)

58021137e3753452276387b7e03eaf31f052d33d
Vyatcheslav Suharnikov <arz.freezy@gmail.com>
2019-10-30 13:10:55 +0300

    DEX-436 A separate detailed log for DEX in integration tests

--- dex-it-tools/src/main/scala/com/wavesplatform/dex/it/docker/Docker.scala
+++ dex-it-tools/src/main/scala/com/wavesplatform/dex/it/docker/Docker.scala
@@ -14,13 +14,15 @@ import cats.instances.map._
 import cats.instances.string._
 import cats.kernel.Monoid
 import com.google.common.primitives.Ints._
+import com.spotify.docker.client.exceptions.ContainerNotFoundException
 import com.spotify.docker.client.messages.EndpointConfig.EndpointIpamConfig
 import com.spotify.docker.client.messages._
 import com.spotify.docker.client.shaded.com.google.common.collect.ImmutableList
 import com.spotify.docker.client.{DefaultDockerClient, DockerClient}
 import com.wavesplatform.utils.ScorexLogging
 import monix.eval.Coeval
-import org.apache.commons.compress.archivers.tar.{TarArchiveEntry, TarArchiveOutputStream}
+import org.apache.commons.compress.archivers.tar.{TarArchiveEntry, TarArchiveInputStream, TarArchiveOutputStream}
+import org.apache.commons.io.IOUtils
 
 import scala.collection.JavaConverters._
 import scala.util.Random
@@ -141,6 +143,19 @@ class Docker(suiteName: String = "") extends AutoCloseable with ScorexLogging {
     try client.copyToContainer(is, container.id, s"${to.getParent.toString}")
     finally is.close()
   }
+
+  def loadFile(container: DockerContainer, containerPath: Path, toLocal: Path): Unit =
+    try {
+      val is = client.archiveContainer(container.id, containerPath.toString)
+      val s  = new TarArchiveInputStream(is)
+      Iterator.continually(s.getNextEntry).takeWhile(_ != null).take(1).foreach { _ =>
+        val out = new FileOutputStream(toLocal.toString)
+        try IOUtils.copy(s, out)
+        finally out.close()
+      }
+    } catch {
+      case e: ContainerNotFoundException => log.warn(s"File '$containerPath' not found: ${e.getMessage}")
+    }
 
   // TODO remove
   def start(container: Coeval[DockerContainer]): Unit = start { container() }
@@ -304,6 +319,15 @@ class Docker(suiteName: String = "") extends AutoCloseable with ScorexLogging {
     } finally {
       fileStream.close()
     }
+
+    val containerSystemLogPath = Paths.get(container.basePath, "system.log")
+    val localSystemLogPath     = logDir().resolve(s"container-${container.name}.system.log")
+    log.info(s"${prefix(container)} Loading system log from '$containerSystemLogPath' to '$localSystemLogPath'")
+    loadFile(
+      container,
+      containerPath = containerSystemLogPath,
+      toLocal = localSystemLogPath
+    )
   }
 
   private def endpointConfigFor(number: Int, netAlias: Option[String]): EndpointConfig = {

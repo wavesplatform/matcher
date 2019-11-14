8a0314387bf1978729365cbc82b196e002af5c64
DenisSerebryansky <40212703+DenisSerebryansky@users.noreply.github.com>
2019-11-14 16:10:09 +0300

    DEX-401 Custom further processing of the loading results in gRPC caches (#95)

--- waves-integration/src/main/scala/com/wavesplatform/dex/grpc/integration/caches/BalancesCache.scala
+++ waves-integration/src/main/scala/com/wavesplatform/dex/grpc/integration/caches/BalancesCache.scala
@@ -6,9 +6,10 @@ import com.wavesplatform.transaction.Asset
 import scala.concurrent.{ExecutionContext, Future}
 
 class BalancesCache(loader: (Address, Asset) => Future[Long])(implicit executionContext: ExecutionContext)
-    extends BlockchainCache[(Address, Asset), Future[BigInt]](
+    extends BlockchainCache[(Address, Asset), BigInt](
       loader = { case (address, asset) => loader(address, asset) map BigInt.apply },
-      expiration = None
+      expiration = None,
+      invalidationPredicate = BlockchainCache.noCustomInvalidationLogic
     ) {
 
   def batchPut(batch: Map[Address, Map[Asset, Long]]): Unit = {

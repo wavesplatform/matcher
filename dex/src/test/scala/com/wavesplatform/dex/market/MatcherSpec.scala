1d6f0b2299a25acfcdc31cde1358b4e8d179107d
Vyatcheslav Suharnikov <arz.freezy@gmail.com>
2019-11-19 11:56:59 +0300

    DEX-469 New installation instructions
    * Removed outdated settings;
    * Removed not used dependencies;
    * Removed outdated classes;

--- dex/src/test/scala/com/wavesplatform/dex/market/MatcherSpec.scala
+++ dex/src/test/scala/com/wavesplatform/dex/market/MatcherSpec.scala
@@ -1,8 +1,7 @@
 package com.wavesplatform.dex.market
 
 import akka.actor.ActorSystem
-import akka.persistence.inmemory.extension.{InMemoryJournalStorage, InMemorySnapshotStorage, StorageExtension}
-import akka.testkit.{TestKitBase, TestProbe}
+import akka.testkit.TestKitBase
 import com.typesafe.config.ConfigFactory
 import com.wavesplatform.settings.loadConfig
 import com.wavesplatform.utils.ScorexLogging
@@ -21,15 +20,6 @@ trait MatcherSpecLike extends TestKitBase with Matchers with BeforeAndAfterAll w
     actorSystemName,
     loadConfig(ConfigFactory.empty())
   )
-
-  override protected def beforeEach(): Unit = {
-    val p = TestProbe()
-    p.send(StorageExtension(system).journalStorage, InMemoryJournalStorage.ClearJournal)
-    p.expectMsg(akka.actor.Status.Success(""))
-    p.send(StorageExtension(system).snapshotStorage, InMemorySnapshotStorage.ClearSnapshots)
-    p.expectMsg(akka.actor.Status.Success(""))
-    super.beforeEach()
-  }
 
   override protected def afterAll(): Unit = {
     super.afterAll()

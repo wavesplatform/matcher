d667c680bbffe1349731207a466a35b7700620d5
Vyatcheslav Suharnikov <arz.freezy@gmail.com>
2019-11-29 15:58:48 +0300

    DEX-488 Mass cancel issue
    Creation of topic before test runs

--- dex-it/src/test/scala/com/wavesplatform/it/MatcherSuiteBase.scala
+++ dex-it/src/test/scala/com/wavesplatform/it/MatcherSuiteBase.scala
@@ -1,15 +1,18 @@
 package com.wavesplatform.it
 
+import java.util.Properties
 import java.util.concurrent.ThreadLocalRandom
 
 import com.typesafe.config.{Config, ConfigFactory}
-import com.wavesplatform.it.MatcherSuiteBase.baseConfig
+import com.wavesplatform.it.MatcherSuiteBase._
 import com.wavesplatform.it.sync.config.MatcherPriceAssetConfig
 import com.wavesplatform.it.transactions.NodesFromDocker
 import com.wavesplatform.it.util._
+import org.apache.kafka.clients.admin.{AdminClient, NewTopic}
 import org.scalatest._
 import org.scalatest.concurrent.Eventually
 
+import scala.collection.JavaConverters._
 import scala.concurrent.ExecutionContext
 import scala.concurrent.duration.DurationInt
 
@@ -40,10 +43,12 @@ abstract class MatcherSuiteBase
   val smartTradeFee    = tradeFee + smartFee
   val twoSmartTradeFee = tradeFee + 2 * smartFee
 
+  private val topicName = s"dex-${ThreadLocalRandom.current().nextInt(0, Int.MaxValue)}"
+
   protected override def createDocker: Docker = new Docker(
     imageName = "com.wavesplatform/dex-it:latest",
     tag = getClass.getSimpleName,
-    suiteConfig = baseConfig(ThreadLocalRandom.current().nextInt(0, Int.MaxValue))
+    suiteConfig = baseConfig(topicName)
   )
 
   protected def node = dockerNodes().head
@@ -59,12 +64,15 @@ abstract class MatcherSuiteBase
       "org.asynchttpclient.requestTimeout"  -> "120000",
       "org.asynchttpclient.ioThreadsCount"  -> "15"
     ).foreach(Function.tupled(System.setProperty))
+
+    createKafkaTopic(topicName)
     super.beforeAll()
   }
+
 }
 
 object MatcherSuiteBase {
-  private def baseConfig(seed: Int): Config = Option(System.getenv("KAFKA_SERVER")).fold(ConfigFactory.empty()) { kafkaServer =>
+  private def baseConfig(topicName: String): Config = kafkaServer.fold(ConfigFactory.empty()) { kafkaServer =>
     ConfigFactory.parseString(s"""
          |logback.configurationFile=/opt/waves/logback-container.xml
          |
@@ -72,8 +80,29 @@ object MatcherSuiteBase {
          |  type = kafka
          |  kafka {
          |    servers = "$kafkaServer"
-         |    topic = "dex-$seed"
+         |    topic = "$topicName"
          |  }
          |}""".stripMargin)
   }
+
+  private def createKafkaTopic(name: String): Unit = kafkaServer.foreach { server =>
+    val properties = new Properties()
+    properties.putAll(
+      Map(
+        "bootstrap.servers"  -> server,
+        "group.id"           -> s"create-$name",
+        "key.deserializer"   -> "org.apache.kafka.common.serialization.StringDeserializer",
+        "value.deserializer" -> "org.apache.kafka.common.serialization.StringDeserializer"
+      ).asJava)
+
+    val adminClient = AdminClient.create(properties)
+    try {
+      val newTopic = new NewTopic(name, 0, 0.toShort)
+      adminClient.createTopics(java.util.Collections.singletonList(newTopic))
+    } finally {
+      adminClient.close()
+    }
+  }
+
+  private def kafkaServer: Option[String] = Option(System.getenv("KAFKA_SERVER"))
 }

cc045ba912c77bd7c4b3ba6884670099e067e067
Denis Serebryansky <denis.serebryansky.eu@gmail.com>
2019-12-06 16:38:39 +0300

    DEX-528 Dependencies refactoring

--- project/Network.scala
+++ project/Network.scala
@@ -1,20 +1,20 @@
 sealed abstract class NodeNetwork(val suffix: String) {
   lazy val packageSuffix = if (suffix == Mainnet.suffix) "" else "-" + suffix
-  override val toString = suffix
+  override val toString  = suffix
 }
 
 object NodeNetwork {
-  def apply(v: Option[String]) = v match {
-    case Some(Testnet.suffix) => Testnet
+  def apply(v: Option[String]): NodeNetwork = v match {
+    case Some(Testnet.suffix)  => Testnet
     case Some(Stagenet.suffix) => Stagenet
-    case Some(Devnet.suffix) => Devnet
-    case _ => Mainnet
+    case Some(Devnet.suffix)   => Devnet
+    case _                     => Mainnet
   }
-  
+
   val All: List[NodeNetwork] = List(Mainnet, Testnet, Stagenet, Devnet)
 }
 
-object Mainnet extends NodeNetwork("mainnet")
-object Testnet extends NodeNetwork("testnet")
+object Mainnet  extends NodeNetwork("mainnet")
+object Testnet  extends NodeNetwork("testnet")
 object Stagenet extends NodeNetwork("stagenet")
-object Devnet extends NodeNetwork("devnet")
+object Devnet   extends NodeNetwork("devnet")

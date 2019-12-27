9c7b4e068e077317c13f6c8be75b122946069aef
Vyatcheslav Suharnikov <arz.freezy@gmail.com>
2019-12-27 14:24:51 +0300

    DEX-539 Merge 1.1.x into master
    * Rewrote OrderFixedFeeTestSuite

--- dex-it/src/test/scala/com/wavesplatform/it/sync/orders/OrderFixedFeeTestSuite.scala
+++ dex-it/src/test/scala/com/wavesplatform/it/sync/orders/OrderFixedFeeTestSuite.scala
@@ -2,242 +2,125 @@ package com.wavesplatform.it.sync.orders
 
 import com.typesafe.config.Config
 import com.typesafe.config.ConfigFactory.parseString
-import com.wavesplatform.account.PublicKey
-import com.wavesplatform.common.state.ByteStr
 import com.wavesplatform.common.utils.EitherExt2
+import com.wavesplatform.dex.it.api.responses.dex.OrderStatus
+import com.wavesplatform.dex.it.waves.MkWavesEntities.IssueResults
 import com.wavesplatform.it.MatcherSuiteBase
-import com.wavesplatform.lang.script.Script
 import com.wavesplatform.lang.script.v1.ExprScript
 import com.wavesplatform.lang.v1.compiler.Terms
-import com.wavesplatform.transaction.Asset.{IssuedAsset, Waves}
-import com.wavesplatform.transaction.assets.exchange.{AssetPair, Order}
+import com.wavesplatform.transaction.Asset
+import com.wavesplatform.transaction.Asset.Waves
+import com.wavesplatform.transaction.assets.exchange.{AssetPair, OrderType}
 
-import scala.concurrent.duration._
+class OrderFixedFeeTestSuite extends MatcherSuiteBase {
+  private val minMatcherFee = 200000L
+  private val price         = 100000000L
 
-class OrderFixedFeeTestSuite extends MatcherSuiteBase with NTPTime {
+  private val IssueResults(issueAliceAssetTx, _, aliceAsset) = mkIssueExtended(alice, "AliceCoin", quantity = 9999999999999L, decimals = 0)
+  private val IssueResults(issueAliceScriptedAssetTx, _, aliceScriptedAsset) =
+    mkIssueExtended(alice,
+                    "AliceSmartAsset",
+                    quantity = 9999999999999L,
+                    decimals = 0,
+                    fee = smartIssueFee,
+                    script = Some(ExprScript(Terms.TRUE).explicitGet()))
 
-  import OrderFixedFeeTestSuite._
-
-  val minMatcherFee = 200000L
-  val price = 100000000L
-  val script: Script = ExprScript(Terms.TRUE).explicitGet()
-  val scriptBase64: String = script.bytes.value.base64
-
-  val aliceAssetBase58: String = node
-    .broadcastIssue(
-      alice,
-      name = "AliceCoin",
-      description = "AliceCoin for matcher's tests",
-      quantity = 9999999999999L,
-      decimals = 0,
-      reissuable = false,
-      fee = smartIssueFee,
-      script = None,
-      waitForTx = true
-    )
-    .id
-  val aliceAsset = IssuedAsset(ByteStr.decodeBase58(aliceAssetBase58).get)
-  val aliceScriptedAssetBase58: String = node
-    .broadcastIssue(
-      alice,
-      name = "AliceSmartAsset",
-      description = "AliceSmartAsset for matcher's tests",
-      quantity = 9999999999999L,
-      decimals = 0,
-      reissuable = false,
-      fee = smartIssueFee,
-      script = Some(scriptBase64),
-      waitForTx = true
-    )
-    .id
-  val aliceScriptedAsset = IssuedAsset(ByteStr.decodeBase58(aliceScriptedAssetBase58).get)
-  val aliceAddress = alice.publicKey.toAddress.stringRepr
-  val bobAddress = bob.publicKey.toAddress.stringRepr
-
-  val matcherPublicKey: PublicKey = matcher.publicKey
+  override protected def beforeAll(): Unit = {
+    super.beforeAll()
+    broadcastAndAwait(issueAliceAssetTx, issueAliceScriptedAssetTx)
+  }
 
   "Matcher" - {
     "when has some asset as fixed fee in config and orders placed" - {
-      "should accept orders if orders' matcherFeeAsset equal to specified in config" in {
-        for (fixedAssetMatcherFee <- Seq(
-          aliceAsset,
-          aliceScriptedAsset
-        )) {
-          val fixedAssetMatcherFeeBase58 = fixedAssetMatcherFee.id.toString
-          docker.restartNode(node,
-            configWithOrderFeeFixed(
-              matcherFeeAssetId = fixedAssetMatcherFeeBase58,
-              minFee = minMatcherFee
-            ))
+      "should accept orders if orders' matcherFeeAsset equal to specified in config" - {
+        List(
+          "regular asset" -> aliceAsset,
+          "scriptedAsset" -> aliceScriptedAsset
+        ).foreach {
+          case (title, asset) =>
+            s"$title" in {
+              val pair        = AssetPair(asset, Waves)
+              val orderAmount = 1
 
-          val someWavesPair = AssetPair(fixedAssetMatcherFee, Waves)
-          node.broadcastTransfer(alice, bobAddress, node.assetBalance(aliceAddress, fixedAssetMatcherFeeBase58).balance / 2, minFee,
-            Some(fixedAssetMatcherFeeBase58), waitForTx = true, feeAssetId = None)
+              dex1.restartWithNewSuiteConfig { configWithOrderFeeFixed(asset) }
+              broadcastAndAwait(mkTransfer(alice, bob, wavesNode1.api.balance(alice, asset), asset))
 
-          val ts = ntpTime.correctedTime()
-          val expirationTimestamp = ts + Order.MaxLiveTime - 10000
-          val orderAmount = 1
-          val aliceAssetBalanceBefore = node.assetBalance(aliceAddress, fixedAssetMatcherFeeBase58).balance
-          val bobAssetBalanceBefore = node.assetBalance(bobAddress, fixedAssetMatcherFeeBase58).balance
-          val aliceWavesBalanceBefore = node.balanceDetails(aliceAddress).available
-          val bobWavesBalanceBefore = node.balanceDetails(bobAddress).available
+              val aliceAssetBalanceBefore = wavesNode1.api.balance(alice, asset)
+              val aliceWavesBalanceBefore = wavesNode1.api.balance(alice, Waves)
 
-          val aliceOrderIdFill = node
-            .placeOrder(
-              Order
-                .buy(
-                  sender = alice,
-                  matcher = matcherPublicKey,
-                  pair = someWavesPair,
-                  amount = orderAmount,
-                  price = price,
-                  timestamp = ts,
-                  expiration = expirationTimestamp,
-                  matcherFee = minMatcherFee,
-                  version = 3,
-                  matcherFeeAssetId = fixedAssetMatcherFee
-                ))
-            .message
-            .id
-          node.waitOrderStatus(someWavesPair, aliceOrderIdFill, "Accepted")
-          node.reservedBalance(alice)("WAVES") shouldBe orderAmount * price / 100000000
-          node.reservedBalance(alice)(s"$fixedAssetMatcherFeeBase58") shouldBe minMatcherFee - orderAmount
-          node.tradableBalance(alice, someWavesPair)("WAVES") shouldBe aliceWavesBalanceBefore - (orderAmount * price / 100000000)
-          node.tradableBalance(alice, someWavesPair)(s"$fixedAssetMatcherFeeBase58") shouldBe aliceAssetBalanceBefore - (minMatcherFee - orderAmount)
+              val bobAssetBalanceBefore = wavesNode1.api.balance(bob, asset)
+              val bobWavesBalanceBefore = wavesNode1.api.balance(bob, Waves)
 
-          val bobSellOrderId = node
-            .placeOrder(
-              Order
-                .sell(
-                  sender = bob,
-                  matcher = matcherPublicKey,
-                  pair = someWavesPair,
-                  amount = orderAmount,
-                  price = price,
-                  timestamp = ts,
-                  expiration = expirationTimestamp,
-                  matcherFee = minMatcherFee,
-                  version = 3,
-                  matcherFeeAssetId = fixedAssetMatcherFee
-                ))
-            .message
-            .id
+              val aliceOrder = mkOrder(alice, pair, OrderType.BUY, orderAmount, price, matcherFee = minMatcherFee, matcherFeeAssetId = asset)
+              placeAndAwait(aliceOrder)
 
-          node.waitOrderStatus(someWavesPair, aliceOrderIdFill, "Filled", waitTime = 2.minutes)
-          node.waitOrderStatus(someWavesPair, bobSellOrderId, "Filled", waitTime = 2.minutes)
+              val reservedBalance1 = dex1.api.reservedBalance(alice)
+              reservedBalance1(Waves) shouldBe orderAmount * price / 100000000
+              reservedBalance1(asset) shouldBe minMatcherFee - orderAmount
 
-          node.waitOrderInBlockchain(aliceOrderIdFill)
+              val tradableBalance1 = dex1.api.tradableBalance(alice, pair)
+              tradableBalance1(Waves) shouldBe aliceWavesBalanceBefore - (orderAmount * price / 100000000)
+              tradableBalance1(asset) shouldBe aliceAssetBalanceBefore - (minMatcherFee - orderAmount)
 
-          node.assetBalance(aliceAddress, fixedAssetMatcherFeeBase58).balance shouldBe aliceAssetBalanceBefore - minMatcherFee + orderAmount
-          node.assetBalance(bobAddress, fixedAssetMatcherFeeBase58).balance shouldBe bobAssetBalanceBefore - minMatcherFee - orderAmount
-          node.balanceDetails(aliceAddress).available shouldBe aliceWavesBalanceBefore - (orderAmount * price / 100000000)
-          node.balanceDetails(bobAddress).available shouldBe bobWavesBalanceBefore + (orderAmount * price / 100000000)
+              val bobOrder = mkOrder(bob, pair, OrderType.SELL, orderAmount, price, matcherFee = minMatcherFee, matcherFeeAssetId = asset)
+              dex1.api.place(bobOrder)
+
+              dex1.api.waitForOrderStatus(aliceOrder, OrderStatus.Filled)
+              dex1.api.waitForOrderStatus(bobOrder, OrderStatus.Filled)
+
+              waitForOrderAtNode(aliceOrder)
+
+              wavesNode1.api.balance(alice, asset) shouldBe aliceAssetBalanceBefore - minMatcherFee + orderAmount
+              wavesNode1.api.balance(alice, Waves) shouldBe aliceWavesBalanceBefore - (orderAmount * price / 100000000)
+
+              wavesNode1.api.balance(bob, asset) shouldBe bobAssetBalanceBefore - minMatcherFee - orderAmount
+              wavesNode1.api.balance(bob, Waves) shouldBe bobWavesBalanceBefore + (orderAmount * price / 100000000)
+            }
         }
       }
 
       "should accept orders if sender received amount > than fee amount" in {
-        docker.restartNode(node,
-          configWithOrderFeeFixed(
-            matcherFeeAssetId = aliceAssetBase58,
-            minFee = minMatcherFee))
+        dex1.restartWithNewSuiteConfig { configWithOrderFeeFixed(aliceAsset) }
+        broadcastAndAwait(mkTransfer(bob, alice, wavesNode1.api.balance(bob, aliceAsset), aliceAsset))
 
-        node.broadcastTransfer(
-          bob,
-          aliceAddress,
-          node.assetBalance(bobAddress, aliceAssetBase58).balance,
-          minFee,
-          Some(aliceAssetBase58),
-          None,
-          waitForTx = true)
-
-        val someWavesPair = AssetPair(aliceAsset, Waves)
-
-        val ts = ntpTime.correctedTime()
-        val expirationTimestamp = ts + Order.MaxLiveTime - 10000
-
-        val bobOrderId = node
-          .placeOrder(
-            Order
-              .buy(
-                sender = bob,
-                matcher = matcherPublicKey,
-                pair = someWavesPair,
-                amount = minMatcherFee,
-                price = price,
-                timestamp = ts,
-                expiration = expirationTimestamp,
-                matcherFee = minMatcherFee,
-                version = 3,
-                matcherFeeAssetId = aliceAsset
-              ))
-          .message
-          .id
-
-        node.waitOrderStatus(someWavesPair, bobOrderId, "Accepted")
+        val pair = AssetPair(aliceAsset, Waves)
+        placeAndAwait(mkOrder(bob, pair, OrderType.BUY, amount = minMatcherFee, price, matcherFee = minMatcherFee, matcherFeeAssetId = aliceAsset))
       }
 
       "should reject orders if orders' matcherFeeAsset not equal to specified in config" in {
-        val ts = ntpTime.correctedTime()
-        val expirationTimestamp = ts + Order.MaxLiveTime
-        val aliceWavesPair = AssetPair(aliceAsset, Waves)
-        val buy = Order.buy(alice, matcherPublicKey, aliceWavesPair, 1, price, ts, expirationTimestamp, minMatcherFee, version = 3, Waves)
-        val sell = Order.sell(bob, matcherPublicKey, aliceWavesPair, 1, price, ts, expirationTimestamp, minMatcherFee, version = 3, Waves)
-
-        assertBadRequestAndMessage(node.placeOrder(buy), "")
-        assertBadRequestAndMessage(node.placeOrder(sell), "")
+        val pair = AssetPair(aliceAsset, Waves)
+        dex1.api.tryPlace(mkOrder(alice, pair, OrderType.BUY, 1, price)) shouldBe 'left
+        dex1.api.tryPlace(mkOrder(bob, pair, OrderType.SELL, 1, price)) shouldBe 'left
       }
 
       "should reject orders if orders' matcherFee less than specified minFee in config" in {
-        val ts = ntpTime.correctedTime()
-        val expirationTimestamp = ts + Order.MaxLiveTime
-        val amount = 1
-        val aliceWavesPair = AssetPair(aliceAsset, Waves)
+        val amount                 = 1
+        val pair                   = AssetPair(aliceAsset, Waves)
         val insufficientMatcherFee = minMatcherFee - 1
-        val buy = Order.buy(alice,
-          matcherPublicKey,
-          aliceWavesPair,
-          amount,
-          price,
-          ts,
-          expirationTimestamp,
-          insufficientMatcherFee,
-          version = 3,
-          aliceAsset)
-        val sell = Order.sell(bob,
-          matcherPublicKey,
-          aliceWavesPair,
-          amount,
-          price,
-          ts,
-          expirationTimestamp,
-          insufficientMatcherFee,
-          version = 3,
-          aliceAsset)
 
-        assertBadRequestAndMessage(node.placeOrder(buy),
-          f"Required $minMatcherFee $aliceAssetBase58 as fee for this order, but given $insufficientMatcherFee")
-        assertBadRequestAndMessage(node.placeOrder(sell),
-          f"Required $minMatcherFee $aliceAssetBase58 as fee for this order, but given $insufficientMatcherFee")
+        dex1.api.tryPlace(mkOrder(alice, pair, OrderType.BUY, amount, price, matcherFee = insufficientMatcherFee, matcherFeeAssetId = aliceAsset)) should failWith(
+          9441542, // FeeNotEnough
+          s"Required $minMatcherFee ${AssetPair.assetIdStr(aliceAsset)}"
+        )
 
+        dex1.api.tryPlace(mkOrder(bob, pair, OrderType.SELL, amount, price, matcherFee = insufficientMatcherFee, matcherFeeAssetId = aliceAsset)) should failWith(
+          9441542, // FeeNotEnough
+          s"Required $minMatcherFee ${AssetPair.assetIdStr(aliceAsset)}"
+        )
       }
     }
   }
-}
 
-object OrderFixedFeeTestSuite {
-  def configWithOrderFeeFixed(matcherFeeAssetId: String = "WAVES", minFee: Long): Config = {
-    parseString(
-      s"""
-         |waves.dex {
-         | allowed-order-versions = [1, 2, 3]
-         |  order-fee {
-         |    mode = fixed
-         |
-         |    fixed {
-         |      asset = "$matcherFeeAssetId"
-         |      min-fee = $minFee
-         |   }
-         |  }
-         |}""".stripMargin)
+  private def configWithOrderFeeFixed(matcherFeeAsset: Asset): Config = {
+    parseString(s"""waves.dex {
+                   | allowed-order-versions = [1, 2, 3]
+                   |  order-fee {
+                   |    mode = fixed
+                   |    fixed {
+                   |      asset = ${AssetPair.assetIdStr(matcherFeeAsset)}
+                   |      min-fee = $minMatcherFee
+                   |   }
+                   |  }
+                   |}""".stripMargin)
   }
 }

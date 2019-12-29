5a295f70d68387013f1a3e0d51cb732f14b6c5d3
Vyatcheslav Suharnikov <arz.freezy@gmail.com>
2019-12-29 21:25:47 +0300

    DEX-539 Merge 1.1.x into master
    * Creation of kafka topic before a test runs

--- dex-it/src/test/scala/com/wavesplatform/it/MatcherSuiteBase.scala
+++ dex-it/src/test/scala/com/wavesplatform/it/MatcherSuiteBase.scala
@@ -1,5 +1,7 @@
 package com.wavesplatform.it
 
+import java.util.Properties
+
 import com.wavesplatform.dex.it.api.BaseContainersKit
 import com.wavesplatform.dex.it.api.dex.HasDex
 import com.wavesplatform.dex.it.api.node.HasWavesNode
@@ -11,10 +13,12 @@ import com.wavesplatform.dex.it.waves.{MkWavesEntities, WavesFeeConstants}
 import com.wavesplatform.dex.test.matchers.DiffMatcherWithImplicits
 import com.wavesplatform.it.api.ApiExtensions
 import com.wavesplatform.utils.ScorexLogging
+import org.apache.kafka.clients.admin.{AdminClient, NewTopic}
 import org.scalatest._
 import org.scalatest.concurrent.Eventually
 
 import scala.concurrent.duration.DurationInt
+import scala.collection.JavaConverters._
 
 abstract class MatcherSuiteBase
     extends FreeSpec
@@ -45,7 +49,9 @@ abstract class MatcherSuiteBase
 
   override protected def beforeAll(): Unit = {
     log.debug(s"Perform beforeAll")
-    // TODO it may be required to create a topic before
+    kafkaServer.foreach { _ =>
+      createKafkaTopic(dexRunConfig.getString("waves.dex.events-queue.kafka.topic"))
+    }
     wavesNode1.start()
     dex1.start()
   }
@@ -55,4 +61,25 @@ abstract class MatcherSuiteBase
     stopBaseContainers()
     super.afterAll()
   }
+
+  private def createKafkaTopic(name: String): Unit = kafkaServer.foreach { server =>
+    val properties = new Properties()
+    properties.putAll(
+      Map(
+        "bootstrap.servers"  -> server,
+        "group.id"           -> s"create-$name",
+        "key.deserializer"   -> "org.apache.kafka.common.serialization.StringDeserializer",
+        "value.deserializer" -> "org.apache.kafka.common.serialization.StringDeserializer"
+      ).asJava)
+
+    val adminClient = AdminClient.create(properties)
+    try {
+      val newTopic = new NewTopic(name, 1, 1.toShort)
+      adminClient.createTopics(java.util.Collections.singletonList(newTopic))
+    } finally {
+      adminClient.close()
+    }
+  }
+
+  private def kafkaServer: Option[String] = Option(System.getenv("KAFKA_SERVER"))
 }

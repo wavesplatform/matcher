ec88a28942c670493d27781d351b35cee4c1b3d5
Vyatcheslav Suharnikov <arz.freezy@gmail.com>
2020-01-14 15:46:37 +0300

    DEX-526 Node dependency removing
    * fixed issue with issue transaction parsing during Waves Node start;

--- dex-it-common/src/main/scala/com/wavesplatform/dex/it/json/package.scala
+++ dex-it-common/src/main/scala/com/wavesplatform/dex/it/json/package.scala
@@ -1,32 +1,31 @@
 package com.wavesplatform.dex.it
 
-import com.fasterxml.jackson.core.JsonFactory
-import com.fasterxml.jackson.databind.deser.DefaultDeserializationContext
 import com.wavesplatform.dex.domain.account.AddressScheme
 import com.wavesplatform.dex.domain.asset.{Asset, AssetPair}
 import com.wavesplatform.dex.domain.bytes.ByteStr
 import com.wavesplatform.dex.domain.order.Order
 import com.wavesplatform.wavesj.Transaction
 import com.wavesplatform.wavesj.json.WavesJsonMapper
-import com.wavesplatform.wavesj.json.deser.TransactionDeserializer
 import com.wavesplatform.wavesj.transactions.ExchangeTransaction
 import play.api.libs.json._
+import play.api.libs.json.jackson.PlayJsonModule
 
-import scala.util.{Failure, Success}
+import scala.util.{Failure, Success, Try}
 
 package object json {
 
-  private val mapper         = new WavesJsonMapper(AddressScheme.current.chainId)
-  private val txDeserializer = new TransactionDeserializer(mapper)
-  private val jsonFactory    = new JsonFactory()
+  private val mapper = new WavesJsonMapper(AddressScheme.current.chainId)
+  mapper.registerModule(new PlayJsonModule(JsonParserSettings()))
 
-  private def deserializeTx(json: JsValue): Transaction = {
-//    txDeserializer.deserialize(jsonFactory.createParser(json.toString), DefaultDeserializationContext.Impl)
-    mapper.readValue(jsonFactory.createParser(json.toString), classOf[Transaction])
-  }
+  private def wavesJDeserializeTx(json: JsValue): Transaction = mapper.readValue(json.toString, classOf[Transaction])
 
   implicit val transactionFormat: Format[Transaction] = Format[Transaction](
-    Reads(json => JsSuccess { deserializeTx(json) }),
+    Reads { json =>
+      Try(wavesJDeserializeTx(json)) match {
+        case Success(x) => JsSuccess(x)
+        case Failure(e) => JsError(e.getMessage)
+      }
+    },
     Writes(tx => Json.parse(mapper.writeValueAsString(tx)))
   )
 

fc311c1fbe86cae8aad792d29b16e42a0ca8771d
Gadiyak Nikita <ngadiyak@web3tech.ru>
2020-02-18 16:06:18 +0300

    initital

--- dex-it/src/test/scala/com/wavesplatform/it/sync/api/MatcherWebSocketsTestSuite.scala
+++ dex-it/src/test/scala/com/wavesplatform/it/sync/api/MatcherWebSocketsTestSuite.scala
@@ -1,38 +1,24 @@
 package com.wavesplatform.it.sync.api
 
-import akka.Done
-import akka.actor.ActorSystem
-import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.ws.{Message, TextMessage, WebSocketRequest}
-import akka.stream.Materializer
-import akka.stream.scaladsl.{Flow, Keep, Sink, Source}
+import com.wavesplatform.dex.it.api.dex.WebSocket
 import com.wavesplatform.it.MatcherSuiteBase
 
-import scala.concurrent.{Future, Promise}
+import scala.collection.JavaConversions._
 
 class MatcherWebSocketsTestSuite extends MatcherSuiteBase {
 
-  "simple web socket test" in {
+  "connection should be established" in {
+    // open connection to ws-service, connection will be alive until ws.success() will not called
+    val wsAlice = new WebSocket(alice, dex1)
 
-    implicit val system: ActorSystem        = ActorSystem()
-    implicit val materializer: Materializer = Materializer.matFromSystem(system)
+    // some actions...
+    Thread.sleep(10000)
 
-    val sink: Sink[Message, Future[Done]] = Sink.foreach {
-      case message: TextMessage.Strict => message.text should startWith("Now is")
-      case e                           => log.error(s"Impossible: $e")
-    }
+    wsAlice.messages should have size 9
+    for (e <- wsAlice.messages.entrySet) e.getValue should startWith("Now")
 
-    // using Source.maybe materializes into a promise
-    // which will allow us to complete the source later
-    // see https://doc.akka.io/docs/akka-http/current/client-side/websocket-support.html#half-closed-websockets
-    val flow: Flow[Message, Message, Promise[Option[Message]]] = Flow.fromSinkAndSourceMat(sink, Source.maybe[Message])(Keep.right)
-
-    // upgradeResponse is a Future[WebSocketUpgradeResponse] that
-    // completes or fails when the connection succeeds or fails
-    val (_, closed) = Http().singleWebSocketRequest(WebSocketRequest(s"ws://127.0.0.1:${dex1.restApiAddress.getPort}/ws/time"), flow)
-
-    // at some later time we want to disconnect
-    Thread.sleep(3000)
-    closed.success(None)
+    // close the connection
+    wsAlice.success()
   }
+
 }

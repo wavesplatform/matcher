84f6f04b91b10a8b4a9ee27201486092ba7cbeef
Denis Serebryansky <denis.serebryansky.eu@gmail.com>
2020-03-11 18:17:05 +0300

    DEX-630 Authenticated stream WS unsubscription
     * AddressActor removes subscriber after closing the connection

--- dex/src/test/scala/com/wavesplatform/dex/api/RouteSpec.scala
+++ dex/src/test/scala/com/wavesplatform/dex/api/RouteSpec.scala
@@ -4,6 +4,6 @@ import akka.http.scaladsl.testkit._
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.should.Matchers
 
-abstract class RouteSpec(basePath: String) extends AnyFreeSpec with ScalatestRouteTest with Matchers /* with ApiErrorMatchers */ {
+abstract class RouteSpec(basePath: String) extends AnyFreeSpec with ScalatestRouteTest with Matchers {
   protected def routePath(suffix: String) = s"$basePath$suffix"
 }

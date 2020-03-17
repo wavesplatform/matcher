67b4ee7106c54d67e8dffb92dffd29be5849aff6
DenisSerebryansky <40212703+DenisSerebryansky@users.noreply.github.com>
2020-03-17 12:47:57 +0300

    DEX-656 Timestamp in web socket messages (#198)
    * WsAddressState and WsOrderBook got timestamp
    * Diffx implicits that ignore timestamp added
    * WebSocketConnection logs any messages
    * WebSocketMessagesSerdeSpecification improved

--- dex-it-common/src/main/scala/com/wavesplatform/dex/it/api/websockets/WebSocketAuthenticatedConnection.scala
+++ dex-it-common/src/main/scala/com/wavesplatform/dex/it/api/websockets/WebSocketAuthenticatedConnection.scala
@@ -1,25 +1,16 @@
 package com.wavesplatform.dex.it.api.websockets
 
 import akka.actor.ActorSystem
-import akka.http.scaladsl.model.ws.Message
 import akka.stream.Materializer
 import com.wavesplatform.dex.api.websockets.{WsAddressState, WsBalances, WsOrder}
 import com.wavesplatform.dex.domain.asset.Asset
-import com.wavesplatform.dex.domain.utils.ScorexLogging
 import play.api.libs.json.Json
 
 class WebSocketAuthenticatedConnection(uri: String)(implicit system: ActorSystem, materializer: Materializer)
-    extends WebSocketConnection[WsAddressState](uri, WebSocketAuthenticatedConnection.parseAndLogMessages, true) {
+    extends WebSocketConnection[WsAddressState](uri, msg => Json.parse(msg.asTextMessage.getStrictText).as[WsAddressState], true) {
 
   def getBalancesChanges: Seq[Map[Asset, WsBalances]] = getMessagesBuffer.map(_.balances).distinct
   def getOrderChanges: Seq[WsOrder]                   = getMessagesBuffer.flatMap(_.orders).distinct
 
   def getSnapshot: WsAddressState = getMessagesBuffer.headOption.getOrElse(WsAddressState.empty)
-}
-
-object WebSocketAuthenticatedConnection extends ScorexLogging {
-  def parseAndLogMessages(msg: Message): WsAddressState = {
-    log.trace(s"Got message: $msg")
-    Json.parse(msg.asTextMessage.getStrictText).as[WsAddressState]
-  }
 }
--- dex-test-common/src/main/scala/com/wavesplatform/dex/test/matchers/DiffMatcherWithImplicits.scala
+++ dex-test-common/src/main/scala/com/wavesplatform/dex/test/matchers/DiffMatcherWithImplicits.scala
@@ -2,14 +2,12 @@ package com.wavesplatform.dex.test.matchers
 
 import com.softwaremill.diffx.{Derived, Diff, DiffResultDifferent, DiffResultValue, FieldPath, Identical}
 import com.wavesplatform.dex.domain.bytes.ByteStr
-import com.wavesplatform.dex.test.matchers.DiffMatcherWithImplicits.DiffForMatcher
+import com.wavesplatform.dex.test.matchers.DiffMatcherWithImplicits.{DiffForMatcher, getDiff}
 import org.scalatest.matchers.{MatchResult, Matcher}
 
 trait DiffMatcherWithImplicits {
 
-  private val byteStrDiff: Diff[ByteStr] = (left: ByteStr, right: ByteStr, _: List[FieldPath]) => {
-    if (left.toString == right.toString) Identical(left) else DiffResultValue(left, right)
-  }
+  private val byteStrDiff: Diff[ByteStr] = getDiff[ByteStr](_.toString == _.toString)
 
   implicit val derivedByteStrDiff: Derived[Diff[ByteStr]] = Derived(byteStrDiff)
 
@@ -17,10 +15,15 @@ trait DiffMatcherWithImplicits {
 }
 
 object DiffMatcherWithImplicits {
+
   case class DiffForMatcher[A: Diff](right: A) extends Matcher[A] {
     override def apply(left: A): MatchResult = Diff[A].apply(left, right) match {
       case c: DiffResultDifferent => MatchResult(matches = false, s"Matching error:\n${Console.RESET}${c.show}${Console.RESET}", "")
       case _                      => MatchResult(matches = true, "", "")
     }
+  }
+
+  def getDiff[T](comparison: (T, T) => Boolean): Diff[T] = { (left: T, right: T, _: List[FieldPath]) =>
+    if (comparison(left, right)) Identical(left) else DiffResultValue(left, right)
   }
 }

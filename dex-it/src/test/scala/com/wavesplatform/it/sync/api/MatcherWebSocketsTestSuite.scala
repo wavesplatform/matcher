9270698c9ca745322386d7ded67fff0e830b9f80
Nikita Gadiyak <ngadiyak@gmail.com>
2020-03-20 17:31:56 +0300

    Delete MatcherWebSocketsTestSuite.scala

--- dex-it/src/test/scala/com/wavesplatform/it/sync/api/MatcherWebSocketsTestSuite.scala
+++
@@ -1,9 +0,0 @@
-package com.wavesplatform.it.sync.api
-
-import com.wavesplatform.dex.it.api.websockets.HasWebSockets
-import com.wavesplatform.it.MatcherSuiteBase
-
-class MatcherWebSocketsTestSuite extends MatcherSuiteBase with HasWebSockets {
-
-  //TODO: remove
-}

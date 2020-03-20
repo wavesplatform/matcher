3913cfb3a3ca99568a4c6452196e23a13227e864
Gadiyak Nikita <ngadiyak@web3tech.ru>
2020-03-20 17:29:24 +0300

    fix

---
+++ dex-it/src/test/scala/com/wavesplatform/it/sync/api/MatcherWebSocketsTestSuite.scala
@@ -0,0 +1,10 @@
+package com.wavesplatform.it.sync.api
+
+import com.wavesplatform.dex.it.api.websockets.HasWebSockets
+import com.wavesplatform.it.MatcherSuiteBase
+
+class MatcherWebSocketsTestSuite extends MatcherSuiteBase with HasWebSockets {
+
+  //TODO: remove
+
+}

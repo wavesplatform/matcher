aa677cbdbb8f763ec09aeb568bcba5568eb2274e
Gadiyak Nikita <ngadiyak@web3tech.ru>
2020-05-29 20:04:50 +0300

    fix

--- dex-load/src/main/scala/Request.scala
+++ dex-load/src/main/scala/Request.scala
@@ -19,7 +19,7 @@ case class Request(httpType: String, path: String, tag: String, obj: ApiJson = n
   )
 
   def mkGet(path: String, tag: String = "", additionalHeaders: Map[String, String] = Map.empty) = {
-    val request = s"GET $path HTTP/1.1\r\n${(defaultHeaders ++ additionalHeaders).map { case (k, v) => s"$k: $v" }.mkString("\r\n")}\r\n\r\n"
+    val request = s"${Request.GET} $path HTTP/1.1\r\n${(defaultHeaders ++ additionalHeaders).map { case (k, v) => s"$k: $v" }.mkString("\r\n")}\r\n\r\n"
 
     s"${request.length} ${tag.toUpperCase}\n$request\r\n"
   }
@@ -33,7 +33,7 @@ case class Request(httpType: String, path: String, tag: String, obj: ApiJson = n
       "X-API-Key"                -> settings.apiKey
     )
 
-    val request = s"POST $path HTTP/1.1\r\n${headers.map { case (k, v) => s"$k: $v" }.mkString("\r\n")}\r\n\r\n$body"
+    val request = s"${Request.POST}  $path HTTP/1.1\r\n${headers.map { case (k, v) => s"$k: $v" }.mkString("\r\n")}\r\n\r\n$body"
 
     s"${request.length} ${tag.toUpperCase}\n$request\r\n"
   }

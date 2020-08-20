fd908dbf43d02a17498dd0889bfd3ab4b01e6cda
Gadiyak Nikita <ngadiyak@web3tech.ru>
2020-08-20 13:56:41 +0300

    remove annotations

--- waves-ext/src/test/scala/com/wavesplatform/dex/WavesExtSuiteBase.scala
+++ waves-ext/src/test/scala/com/wavesplatform/dex/WavesExtSuiteBase.scala
@@ -1,10 +1,7 @@
 package com.wavesplatform.dex
 
-import io.qameta.allure.{Feature, Story}
 import io.qameta.allure.scalatest.AllureScalatestContext
 import org.scalatest.freespec.AnyFreeSpecLike
 import org.scalatest.matchers.should.Matchers
 
-@Story("waves-ext")
-@Feature("tests")
 trait WavesExtSuiteBase extends AnyFreeSpecLike with Matchers with AllureScalatestContext

f593800180eb4db38c2d1abff2d5629f2c220e31
Gadiyak Nikita <ngadiyak@web3tech.ru>
2020-08-21 10:49:40 +0300

    fix

--- dex-load/src/main/resources/prepareLoadProfile.sh
+++ dex-load/src/main/resources/prepareLoadProfile.sh
@@ -1,3 +1,3 @@
-sed -i "6s/.*/ammofile: $(ls | grep requests)/" /home/yatank/loadtest/dexload.yaml
+sed -i "6s/.*/ ammofile: $(ls | grep requests)/" /home/yatank/loadtest/dexload.yaml
 mv $(ls | grep requests) /home/yatank/loadtest/
 docker exec -i da469fa869cd yandex-tank -c dexload.yaml

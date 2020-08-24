9f31694e5fdf68f1e6320c90f3ea4f39a2d51829
Gadiyak Nikita <ngadiyak@web3tech.ru>
2020-08-24 17:47:11 +0300

    fix

--- dex-load/src/main/resources/runLoadTest.sh
+++ dex-load/src/main/resources/runLoadTest.sh
@@ -1,3 +1,4 @@
 sed -i "6s/.*/ ammofile: $(ls | grep requests)/" /home/yatank/loadtest/dexload.yaml
 mv $(ls | grep requests) /home/yatank/loadtest/
+echo "The performance test has been launched, but we made the decision not to print its stdout. Keep patience, it will be finished near $(date -d '+ 9 minutes')"
 docker exec -i da469fa869cd yandex-tank -c dexload.yaml &>/dev/null

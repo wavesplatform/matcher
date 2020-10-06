d91eccaa45fdad90e1aacb598a562674d091f99e
Vyatcheslav Suharnikov <arz.freezy@gmail.com>
2020-10-06 12:05:11 +0300

    DEX-931 Batch events during Matcher start
     * Generators fixes

--- dex-jmh/src/test/scala/com/wavesplatform/dex/model/address/AddressActorStartingBenchmark.scala
+++ dex-jmh/src/test/scala/com/wavesplatform/dex/model/address/AddressActorStartingBenchmark.scala
@@ -1,7 +1,7 @@
 package com.wavesplatform.dex.model.address
 
 import java.nio.charset.StandardCharsets
-import java.util.concurrent.TimeUnit
+import java.util.concurrent.{ThreadLocalRandom, TimeUnit}
 
 import akka.actor.{ActorRef, ActorSystem}
 import akka.pattern.ask
@@ -26,7 +26,6 @@ import org.scalacheck.Gen
 import scala.concurrent.duration._
 import scala.concurrent.{Await, Future}
 import scala.jdk.CollectionConverters._
-import scala.util.Random
 
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 @BenchmarkMode(Array(Mode.AverageTime))
@@ -40,86 +39,69 @@ class AddressActorStartingBenchmark {
 
 object AddressActorStartingBenchmark {
 
-  object OrderEvent extends Enumeration {
-    type OrderEvent = Value
-    val Added, Cancelled, FilledImmediately, FilledEventually, PartiallyFilled, PartiallyFilledAndThenCancelled = Value
-  }
-
   @State(Scope.Thread) class AddressState extends OrderBookGen {
-
-    val orderAddedEventCount: Int                      = 150
-    val orderCancelledEventCount: Int                  = 100
-    val orderFilledImmediatelyCount: Int               = 200
-    val orderFilledEventuallyCount: Int                = 200
-    val orderPartiallyFilledCount: Int                 = 250
-    val orderPartiallyFilledAndThenCancelledCount: Int = 100
-
-    val ordersCount: Int =
-      orderAddedEventCount +
-        orderCancelledEventCount +
-        orderFilledImmediatelyCount +
-        orderFilledEventuallyCount +
-        orderPartiallyFilledCount +
-        orderPartiallyFilledAndThenCancelledCount
 
     val maxPrice: Long      = 1000L * Order.PriceConstant
     val minPrice: Long      = 1L * Order.PriceConstant
     val priceGen: Gen[Long] = Gen.chooseNum(minPrice, maxPrice)
 
-    val owner: KeyPair   = clientGen.sample.get
-    val counter: KeyPair = KeyPair(ByteStr(s"counter".getBytes(StandardCharsets.UTF_8)))
+    val owner: KeyPair             = clientGen.sample.get
+    val counterOrderOwner: KeyPair = KeyPair(ByteStr("counter".getBytes(StandardCharsets.UTF_8)))
 
-    val askGen: Gen[Order] = orderGen(owner, priceGen, OrderType.SELL)
-    val bidGen: Gen[Order] = orderGen(owner, priceGen, OrderType.BUY)
+    val askGen: Gen[Order]             = orderGen(owner, priceGen, OrderType.SELL)
+    val bidGen: Gen[Order]             = orderGen(owner, priceGen, OrderType.BUY)
+    val orderGen: Gen[Order]           = Gen.oneOf(askGen, bidGen)
+    val limitOrderGen: Gen[LimitOrder] = orderGen.map(LimitOrder(_))
 
-    val orders: List[AcceptedOrder] = ordersGen(ordersCount).sample.get
+    val orderAddedGen: Gen[Seq[Events.Event]] = limitOrderGen.map { ao =>
+      Seq(getOrderAddedEvent(ao))
+    }
 
-    val (eventsGroups, _) =
-      Seq(
-        orderAddedEventCount                      -> OrderEvent.Added,
-        orderCancelledEventCount                  -> OrderEvent.Cancelled,
-        orderFilledImmediatelyCount               -> OrderEvent.FilledImmediately,
-        orderFilledEventuallyCount                -> OrderEvent.FilledEventually,
-        orderPartiallyFilledCount                 -> OrderEvent.PartiallyFilled,
-        orderPartiallyFilledAndThenCancelledCount -> OrderEvent.PartiallyFilledAndThenCancelled
-      ).foldLeft((Seq.empty[Seq[Events.Event]], orders)) { case ((result, ordersRemaining), (groupSize, event)) =>
-        val (ordersGroup, newRemaining) = ordersRemaining.splitAt(groupSize)
-        val eventsGroup =
-          event match {
-            case OrderEvent.Added     => ordersGroup.map(getOrderAddedEvent)
-            case OrderEvent.Cancelled => ordersGroup.flatMap { ao => Seq(getOrderAddedEvent(ao), getOrderCancelledEvent(ao)) }
-            case OrderEvent.FilledImmediately =>
-              ordersGroup.flatMap { ao => Seq(getOrderAddedEvent(ao), getOrderExecutedEvent(ao, getCounter(ao, ao.amount))) }
-            case OrderEvent.FilledEventually =>
-              ordersGroup.flatMap { ao =>
-                val firstExecutedEvent  = getOrderExecutedEvent(ao, getCounter(ao, ao.amount / 2))
-                val secondExecutedEvent = getOrderExecutedEvent(firstExecutedEvent.submittedRemaining, getCounter(ao, ao.amount))
-                Seq(getOrderAddedEvent(ao), firstExecutedEvent, secondExecutedEvent)
-              }
-            case OrderEvent.PartiallyFilled =>
-              ordersGroup.flatMap { ao => Seq(getOrderAddedEvent(ao), getOrderExecutedEvent(ao, getCounter(ao, ao.amount / 2))) }
-            case OrderEvent.PartiallyFilledAndThenCancelled =>
-              ordersGroup.flatMap { ao =>
-                val firstExecutedEvent = getOrderExecutedEvent(ao, getCounter(ao, ao.amount / 2))
-                val cancelEvent        = getOrderCancelledEvent(firstExecutedEvent.submittedRemaining)
-                Seq(getOrderAddedEvent(ao), firstExecutedEvent, cancelEvent)
-              }
-          }
-        (result :+ eventsGroup) -> newRemaining
-      }
+    val orderCanceledGen: Gen[Seq[Events.Event]] = limitOrderGen.map { ao =>
+      Seq(getOrderAddedEvent(ao), getOrderCancelledEvent(ao))
+    }
+
+    val orderFilledImmediatelyGen: Gen[Seq[Events.Event]] = limitOrderGen.map { ao =>
+      Seq(getOrderAddedEvent(ao), getOrderExecutedEvent(ao, getCounter(ao, ao.amount)))
+    }
+
+    val orderFilledEventuallyGen: Gen[Seq[Events.Event]] = limitOrderGen.map { ao =>
+      val firstExecutedEvent  = getOrderExecutedEvent(ao, getCounter(ao, ao.amount / 2))
+      val secondExecutedEvent = getOrderExecutedEvent(firstExecutedEvent.submittedRemaining, getCounter(ao, ao.amount))
+      Seq(getOrderAddedEvent(ao), firstExecutedEvent, secondExecutedEvent)
+    }
+
+    val orderPartiallyFilledGen: Gen[Seq[Events.Event]] = limitOrderGen.map { ao =>
+      Seq(getOrderAddedEvent(ao), getOrderExecutedEvent(ao, getCounter(ao, ao.amount / 2)))
+    }
+
+    val orderPartiallyFilledAndThenCancelledGen: Gen[Seq[Events.Event]] = limitOrderGen.map { ao =>
+      val firstExecutedEvent = getOrderExecutedEvent(ao, getCounter(ao, ao.amount / 2))
+      val cancelEvent        = getOrderCancelledEvent(firstExecutedEvent.submittedRemaining)
+      Seq(getOrderAddedEvent(ao), firstExecutedEvent, cancelEvent)
+    }
+
+    val eventsGen = Gen.frequency(
+      150 -> orderAddedGen,
+      100 -> orderCanceledGen,
+      200 -> orderFilledImmediatelyGen,
+      200 -> orderFilledEventuallyGen,
+      250 -> orderPartiallyFilledGen,
+      100 -> orderPartiallyFilledAndThenCancelledGen
+    )
 
     implicit val efc: ErrorFormatterContext = ErrorFormatterContext.from(_ => 8)
 
     def run(): AddressActor.Reply.OrdersStatuses = {
 
-      val system: ActorSystem = ActorSystem("addressActorBenchmark")
+      val system: ActorSystem = ActorSystem(s"addressActorBenchmark-${ThreadLocalRandom.current().nextInt()}")
 
       val addressActor: ActorRef =
         system.actorOf(
           AddressActor.props(
             owner = owner,
             time = new TestTime(),
-            orderDB = new TestOrderDB(1000),
+            orderDB = new TestOrderDB(10000),
             validate = (_, _) => Future.successful(().asRight),
             store = event => Future.successful { Some(QueueEventWithMeta(0L, 0L, event)) },
             started = true,
@@ -128,10 +110,7 @@ object AddressActorStartingBenchmark {
           )
         )
 
-      for {
-        events <- Random.shuffle(eventsGroups)
-        event  <- events
-      } addressActor ! event
+      eventsGen.sample.get.foreach(addressActor ! _)
 
       addressActor ! AddressDirectoryActor.StartWork
 
@@ -160,16 +139,14 @@ object AddressActorStartingBenchmark {
     def getOrderCancelledEvent(ao: AcceptedOrder): Events.OrderCanceled =
       Events.OrderCanceled(ao, Events.OrderCanceledReason.RequestExecuted, System.currentTimeMillis())
 
-    def getCounter(ao: AcceptedOrder, amount: Long): LimitOrder = LimitOrder(ao.order.updateAmount(amount).updateSender(counter))
+    def getCounter(ao: AcceptedOrder, amount: Long): LimitOrder = LimitOrder(ao.order.updateAmount(amount).updateSender(counterOrderOwner))
 
-    def getOrderExecutedEvent(ao: AcceptedOrder, counter: LimitOrder): Events.OrderExecuted = {
-      Events.OrderExecuted(
-        ao,
-        counter,
-        System.currentTimeMillis(),
-        counter.matcherFee,
-        ao.matcherFee
-      )
-    }
+    def getOrderExecutedEvent(ao: AcceptedOrder, counter: LimitOrder): Events.OrderExecuted = Events.OrderExecuted(
+      ao,
+      counter,
+      System.currentTimeMillis(),
+      counter.matcherFee,
+      ao.matcherFee
+    )
   }
 }

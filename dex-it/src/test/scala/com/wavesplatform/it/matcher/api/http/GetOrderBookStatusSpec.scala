fbe6f22aa62051b2c6dfdb224f38a9009e9151cd
Gadiyak Nikita <ngadiyak@web3tech.ru>
2020-11-10 00:25:43 +0300

    DEX-946-order-book-routes

--- dex-it/src/test/scala/com/wavesplatform/it/matcher/api/http/GetOrderBookStatusSpec.scala
+++ dex-it/src/test/scala/com/wavesplatform/it/matcher/api/http/GetOrderBookStatusSpec.scala
@@ -1,9 +1,12 @@
 package com.wavesplatform.it.matcher.api.http
 
 import com.typesafe.config.{Config, ConfigFactory}
+import com.wavesplatform.dex.api.http.entities.{HttpOrderBookStatus, HttpOrderStatus}
 import com.wavesplatform.dex.domain.asset.AssetPair
-import com.wavesplatform.dex.domain.order.OrderType.BUY
+import com.wavesplatform.dex.domain.order.OrderType.{BUY, SELL}
 import com.wavesplatform.dex.it.api.RawHttpChecks
+import com.wavesplatform.dex.model.{LastTrade, LevelAgg}
+import com.wavesplatform.dex.model.OrderStatus.Filled
 import com.wavesplatform.it.MatcherSuiteBase
 import org.scalatest.prop.TableDrivenPropertyChecks
 
@@ -23,16 +26,70 @@ class GetOrderBookStatusSpec extends MatcherSuiteBase with TableDrivenPropertyCh
 
   override protected def beforeAll(): Unit = {
     wavesNode1.start()
+    broadcastAndAwait(IssueBtcTx)
     broadcastAndAwait(IssueUsdTx)
     dex1.start()
   }
 
   "GET /matcher/orderbook/{amountAsset}/{priceAsset}/status " - {
 
-    "debug " in {
+    "should correctly update the status and data of orderbook " in {
 
-      placeAndAwaitAtDex(mkOrder(alice, wavesUsdPair, BUY, 5.waves, 1.usd))
-      val r = dex1.rawApi.getOrderBookStatus(wavesUsdPair)
+      validate200Json(dex1.rawApi.getOrderBookStatus(wavesUsdPair))
+
+      withClue("- bid amount and price (one order)") {
+        placeAndAwaitAtDex(mkOrder(alice, wavesUsdPair, BUY, 5.waves, 1.usd))
+        val status = validate200Json(dex1.rawApi.getOrderBookStatus(wavesUsdPair))
+        status.bid.get should be(1.usd)
+        status.bidAmount.get should be(5.waves)
+      }
+
+      withClue("- bid amount and price (multiple orders)") {
+        placeAndAwaitAtDex(mkOrder(alice, wavesUsdPair, BUY, 10.waves, 1.1.usd))
+        val status = validate200Json(dex1.rawApi.getOrderBookStatus(wavesUsdPair))
+        status.bid.get should be(1.1.usd)
+        status.bidAmount.get should be(10.waves)
+      }
+
+      withClue("- ask amount and price (one order)") {
+        placeAndAwaitAtDex(mkOrder(alice, wavesUsdPair, SELL, 2.waves, 1.9.usd))
+        val status = validate200Json(dex1.rawApi.getOrderBookStatus(wavesUsdPair))
+        status.ask.get should be(1.9.usd)
+        status.askAmount.get should be(2.waves)
+      }
+
+      withClue("- ask amount and price (multiple orders)") {
+        placeAndAwaitAtDex(mkOrder(alice, wavesUsdPair, SELL, 4.waves, 2.usd))
+        val status = validate200Json(dex1.rawApi.getOrderBookStatus(wavesUsdPair))
+        status.ask.get should be(1.9.usd)
+        status.askAmount.get should be(2.waves)
+      }
+
+      withClue("- trade (best sell order partially filled)") {
+        placeAndAwaitAtNode(mkOrder(alice, wavesUsdPair, BUY, 1.waves, 2.usd))
+        val status = validate200Json(dex1.rawApi.getOrderBookStatus(wavesUsdPair))
+        status.ask.get should be(1.9.usd)
+        status.askAmount.get should be(1.waves)
+        status.bid.get should be(1.1.usd)
+        status.bidAmount.get should be(10.waves)
+
+//        //TODO: will be fixed in the morning, want to sleep ))
+//        status.lastPrice.get should be(LastTrade(1.waves, 1.9.usd, BUY))
+//        status.bestBid.get should be(LevelAgg(1.waves, 1.1.usd))
+//        status.bestAsk.get should be(LevelAgg(1.waves, 1.9.usd))
+//      }
+//
+//      withClue("- trade (best sell order fully filled)") {
+//        placeAndAwaitAtNode(mkOrder(alice, wavesUsdPair, BUY, 1.waves, 2.usd))
+//        val status = validate200Json(dex1.rawApi.getOrderBookStatus(wavesUsdPair))
+//        status.ask.get should be(1.9.usd)
+//        status.askAmount.get should be(1.waves)
+//        status.bid.get should be(1.1.usd)
+//        status.bidAmount.get should be(1.waves)
+//        status.lastPrice.get should be(LastTrade(1.9.usd, 1.waves, BUY))
+//        status.bestBid.get should be(LevelAgg(1.waves, 1.1.usd))
+//        status.bestAsk.get should be(LevelAgg(1.waves, 1.9.usd))
+//      }
     }
 
     "should return exception when amount is not a correct base58 string" in { // TODO: ? Create task for change it to matcherError?
@@ -48,37 +105,5 @@ class GetOrderBookStatusSpec extends MatcherSuiteBase with TableDrivenPropertyCh
         validateMatcherError(dex1.rawApi.getOrderBookStatus(AssetPair.createAssetPair(a, p).get), c, e, m)
       }
     }
-
-//    "is returned even there is no such order book" in {
-//      dex1.rawApi.getMarketStatus(bobNotTradedWavesPair).unsafeGet.leftSideValue should matchTo(HttpMarketStatus(None, None, None, None, None, None, None))
-//    }
-//
-//    "should update after trade" in {
-//      val ask = 5.waves
-//      val askAmount = 5000000
-//
-//      val bid = 10.waves
-//      val bidAmount = 10000000
-//
-//      dex1.api.place(mkOrder(bob, bob2WavesPair, SELL, askAmount, ask))
-//
-//      val resp1 = dex1.api.getMarketStatus(bob2WavesPair)
-//      resp1.lastTrade shouldBe None
-//      resp1.bestBid shouldBe None
-//      resp1.bestAsk should matchTo {
-//        Option(LevelAgg(askAmount, ask))
-//      }
-//
-//      placeAndAwaitAtDex(mkOrder(alice, bob2WavesPair, BUY, bidAmount, bid))
-//
-//      val resp2 = dex1.api.getMarketStatus(bob2WavesPair)
-//      resp2.lastTrade should matchTo {
-//        Option(LastTrade(ask, askAmount, BUY))
-//      }
-//      resp2.bestBid should matchTo {
-//        Option(LevelAgg(bidAmount - askAmount, bid))
-//      }
-//      resp2.bestAsk shouldBe None
-//    }
   }
 }

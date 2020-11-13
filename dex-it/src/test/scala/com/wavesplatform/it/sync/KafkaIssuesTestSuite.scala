1c9ef42375d805ce88b1c1de1c0f8e363725e8dd
Gadiyak Nikita <ngadiyak@web3tech.ru>
2020-11-13 16:23:34 +0300

    DEX-290-stop-matcher-on-deserialization-error

--- dex-it/src/test/scala/com/wavesplatform/it/sync/KafkaIssuesTestSuite.scala
+++ dex-it/src/test/scala/com/wavesplatform/it/sync/KafkaIssuesTestSuite.scala
@@ -195,32 +195,6 @@ class KafkaIssuesTestSuite extends WsSuiteBase with HasKafka {
     }
   }
 
-  "Matcher should stop working when got unexpected kafka message" in {
-    val kafkaProducerProps: Properties = {
-      val props = new Properties()
-      props.put("bootstrap.servers", kafka.bootstrapServers)
-      val stringSerializerName = classOf[StringSerializer].getName
-      props.put("key.serializer", stringSerializerName)
-      props.put("value.serializer", stringSerializerName)
-      props
-    }
-    val sendResult = Promise[Unit]()
-    val producer = new KafkaProducer[String, String](kafkaProducerProps)
-    producer.send(
-      new ProducerRecord(topicName, "incorrect_key", "incorrect_value"),
-      (_: RecordMetadata, exception: Exception) =>
-        Option(exception) match {
-          case Some(e) => sendResult.failure(e)
-          case None => sendResult.success(())
-        }
-    )
-    Await.result(sendResult.future, 10.seconds)
-    producer.close()
-    eventually {
-      dex1.getState().getExitCodeLong shouldBe QueueMessageDeserializationError.code
-    }
-  }
-
   private def clearTopic(topicName: String): Unit = {
     val kafkaClient = mkKafkaAdminClient(s"$kafkaIp:9092")
     try {

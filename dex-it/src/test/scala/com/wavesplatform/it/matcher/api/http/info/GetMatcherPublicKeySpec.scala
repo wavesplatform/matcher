c0b0494534731039c5e520d593593d94daebb327
Gadiyak Nikita <ngadiyak@web3tech.ru>
2020-11-26 21:20:01 +0300

    DEX-945-utility-methods-tests

--- dex-it/src/test/scala/com/wavesplatform/it/matcher/api/http/info/GetMatcherPublicKeySpec.scala
+++ dex-it/src/test/scala/com/wavesplatform/it/matcher/api/http/info/GetMatcherPublicKeySpec.scala
@@ -7,12 +7,13 @@ class GetMatcherPublicKeySpec extends MatcherSuiteBase with RawHttpChecks {
 
   override protected def beforeAll(): Unit = {
     wavesNode1.start()
+    broadcastAndAwait(IssueUsdTx)
     dex1.start()
   }
 
   "GET /matcher" - {
     "should return correct public key of matcher" in {
-      validate200Json(dex1.rawApi.getMatcherPublicKey) should be(matcher.publicKey)
+      validate200Json(dex1.rawApi.getMatcherPublicKey) should be(matcher.publicKey.toString)
     }
   }
 }

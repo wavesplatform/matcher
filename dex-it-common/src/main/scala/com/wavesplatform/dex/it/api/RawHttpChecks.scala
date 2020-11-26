e03202c2b7a2b12c265b458ab5d933d235f60418
Nikita Gadiyak <ngadiyak@gmail.com>
2020-11-26 22:01:30 +0300

    DEX-945 Utility methods tests (#411)

--- dex-it-common/src/main/scala/com/wavesplatform/dex/it/api/RawHttpChecks.scala
+++ dex-it-common/src/main/scala/com/wavesplatform/dex/it/api/RawHttpChecks.scala
@@ -6,34 +6,39 @@ import org.scalatest.matchers.should.Matchers
 
 trait RawHttpChecks extends Matchers {
 
-  private def assertResponseContainHeaders[ErrorT, EntityT](r: EnrichedResponse[ErrorT, EntityT], expected: (String, String)*): Unit =
+  private def validateResponseContainHeaders[ErrorT, EntityT](r: EnrichedResponse[ErrorT, EntityT], expected: (String, String)*): Unit =
     expected.foreach(r.response.headers should contain(_))
 
-
-  protected def validate200Json[ErrorT, EntityT](r: EnrichedResponse[ErrorT, EntityT]): EntityT = {
-    r.response.code should be(StatusCodes.Ok)
-    assertResponseContainHeaders(r, "Content-Type" -> "application/json")
+  private def validateResponse[ErrorT, EntityT](r: EnrichedResponse[ErrorT, EntityT], code: Int, contentType: String): EntityT = {
+    r.response.code should be(code)
+    validateResponseContainHeaders(r, "Content-Type" -> contentType)
     r.unsafeGet
   }
 
-  protected def validate201Json[ErrorT, EntityT](r: EnrichedResponse[ErrorT, EntityT]): EntityT = {
-    r.response.code should be(StatusCodes.Created)
-    assertResponseContainHeaders(r, "Content-Type" -> "application/json")
-    r.unsafeGet
-  }
-  
-  def validateIncorrectSignature[ErrorT, EntityT](r: EnrichedResponse[ErrorT, EntityT]) =
-    validateMatcherError(r, StatusCodes.BadRequest, 1051904, "The request has an invalid signature")
+  protected def validate200Hocon[ErrorT, EntityT](r: EnrichedResponse[ErrorT, EntityT]): EntityT =
+    validateResponse(r, StatusCodes.Ok, "application/hocon")
+
+  protected def validate200Json[ErrorT, EntityT](r: EnrichedResponse[ErrorT, EntityT]): EntityT =
+    validateResponse(r, StatusCodes.Ok, "application/json")
+
+  protected def validate201Json[ErrorT, EntityT](r: EnrichedResponse[ErrorT, EntityT]): EntityT =
+    validateResponse(r, StatusCodes.Created, "application/json")
+
+  protected def validate202Json[ErrorT, EntityT](r: EnrichedResponse[ErrorT, EntityT]): EntityT =
+    validateResponse(r, StatusCodes.Accepted, "application/json")
 
   protected def validate301Redirect[ErrorT, EntityT](r: EnrichedResponse[ErrorT, EntityT]): Unit =
     r.response.code should be(StatusCodes.MovedPermanently)
+
+  protected def validateIncorrectSignature[ErrorT, EntityT](r: EnrichedResponse[ErrorT, EntityT]) =
+    validateMatcherError(r, StatusCodes.BadRequest, 1051904, "The request has an invalid signature")
 
   protected def validateAuthorizationError[ErrorT, EntityT](r: EnrichedResponse[ErrorT, EntityT]): Unit =
     validateMatcherError(r, StatusCodes.Forbidden, 106954752, "Provided API key is not correct")
 
   protected def validateMatcherError[ErrorT, EntityT](r: EnrichedResponse[ErrorT, EntityT], code: Int, error: Int, message: String): Unit = {
     r.response.code should be(code)
-    assertResponseContainHeaders(r, "Content-Type" -> "application/json")
+    validateResponseContainHeaders(r, "Content-Type" -> "application/json")
     r.response.body should be leftSideValue
 
     r.tryGet match {
@@ -44,7 +49,7 @@ trait RawHttpChecks extends Matchers {
 
   protected def validate404Exception[ErrorT, EntityT](r: EnrichedResponse[ErrorT, EntityT]): Unit = {
     r.response.code should be(StatusCodes.NotFound)
-    assertResponseContainHeaders(r, "Content-Type" -> "text/plain; charset=UTF-8")
+    validateResponseContainHeaders(r, "Content-Type" -> "text/plain; charset=UTF-8")
     r.response.body should be(Left("The requested resource could not be found but may be available again in the future."))
   }
 
---
+++ dex-it/src/test/scala/com/wavesplatform/it/matcher/api/http/debug/GetMatcherConfigSpec.scala
@@ -0,0 +1,32 @@
+package com.wavesplatform.it.matcher.api.http.debug
+
+import com.typesafe.config.{Config, ConfigFactory}
+import com.wavesplatform.dex.it.api.RawHttpChecks
+import com.wavesplatform.it.MatcherSuiteBase
+import com.wavesplatform.dex.settings.utils.ConfigOps.ConfigOps
+
+class GetMatcherConfigSpec extends MatcherSuiteBase with RawHttpChecks {
+
+  override protected def beforeAll(): Unit = {
+    wavesNode1.start()
+    dex1.start()
+  }
+
+  "GET /matcher/debug/config" - {
+
+    "should return correct config" in {
+      val config = validate200Hocon(dex1.rawApi.getMatcherConfig).rendered
+
+      Set("user", "pass", "seed", "private", "java", "sun", "api").foreach(config should not contain _)
+    }
+
+    "should return correct error with incorrect x-api-key header" in {
+      validateAuthorizationError(dex1.rawApi.getMatcherConfig(Map("X-API_KEY" -> "incorrect")))
+    }
+
+    "should return correct error without x-api-key header" in {
+      validateAuthorizationError(dex1.rawApi.getMatcherConfig(Map.empty))
+    }
+  }
+
+}

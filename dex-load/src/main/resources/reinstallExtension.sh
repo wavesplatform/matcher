6e574bcd77e4642c3d895ce1a911c03d3765b9ed
Gadiyak Nikita <ngadiyak@web3tech.ru>
2020-12-10 09:39:17 +0300

    DEX-1026-sc6

--- dex-load/src/main/resources/reinstallExtension.sh
+++ dex-load/src/main/resources/reinstallExtension.sh
@@ -1,5 +1,6 @@
 systemctl stop waves-devnet.service || true
 dpkg -P waves-dex-extension-devnet || true
 dpkg -i /home/buildagent-matcher/waves-dex-extension-devnet*.deb
+dpkg -i /home/buildagent-matcher/grpc-server-devnet*.deb
 systemctl start waves-devnet
 rm -rf /home/buildagent-matcher/*

21926d2c71f9c483cf6fb35bb5c3103ebcabf8e6
nik <ngadiyak@gmail.com>
2021-01-09 12:52:56 +0300

    DEX-1049-restart-loadcontainer-if-needed

--- dex-load/src/main/resources/runLoadTest.sh
+++ dex-load/src/main/resources/runLoadTest.sh
@@ -2,6 +2,12 @@ sed -i "6s/.*/ ammofile: $(ls | grep requests)/" /home/yatank/loadtest/dexload.y
 mv $(ls | grep requests) /home/yatank/loadtest/
 rm -rf /home/yatank/loadtest/logs/*
 echo "The performance test has been launched, but we made the decision not to print its stdout. Keep patience, it will be finished near $(date -d '+ 9 minutes')"
-docker exec -i cba44f78ac43 yandex-tank -c dexload.yaml > /dev/null
+if [ ! "$(docker ps -q -f name=dexload)" ]; then
+    if [ "$(docker ps -aq -f status=exited -f name=dexload)" ]; then
+        docker rm dexload
+    fi
+    docker run -v $(pwd):/var/loadtest -v /home/ngadiyak/.ssh:/root/.ssh --rm --net host -it --entrypoint /bin/bash -d --name dexload direvius/yandex-tank:latest
+fi
+docker exec -i dexload yandex-tank -c dexload.yaml > /dev/null
 echo "The performance has been finished"
 echo "$(cat /home/yatank/loadtest/logs/lunapark/$(sudo ls /home/yatank/loadtest/logs/lunapark/)finish_status.yaml | grep -Po -m 1 https://overload.yandex.net/[0-9]+)"

c0e5bff1ce26775f836c0ce0d887d598541d776d
nik <ngadiyak@gmail.com>
2021-02-11 11:09:51 +0300

    DEX-1055-load-tests-4

--- dex-load/src/main/resources/reinstallNode.sh
+++ dex-load/src/main/resources/reinstallNode.sh
@@ -1,5 +1,4 @@
 systemctl stop waves-devnet.service || true
-rm -rf /var/lib/waves-devnet/data || true
 dpkg -P waves-dex-extension-devnet || true
 dpkg -P waves-devnet || true
 dpkg -i /home/buildagent-matcher/waves-devnet*.deb

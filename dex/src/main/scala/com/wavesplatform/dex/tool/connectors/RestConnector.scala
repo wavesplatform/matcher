249ff567667b1c86e6ee59296d8180796c057e01
nik <ngadiyak@gmail.com>
2021-03-10 12:48:53 +0300

    DEX-1092-sttp3

--- dex/src/main/scala/com/wavesplatform/dex/tool/connectors/RestConnector.scala
+++ dex/src/main/scala/com/wavesplatform/dex/tool/connectors/RestConnector.scala
@@ -5,7 +5,7 @@ import com.wavesplatform.dex.cli.ErrorOr
 import com.wavesplatform.dex.error.Implicits.ThrowableOps
 import com.wavesplatform.dex.tool.connectors.RestConnector.{ErrorOrJsonResponse, RequestFunction}
 import play.api.libs.json.{JsValue, Json}
-import sttp.client3.{basicRequest, _}
+import sttp.client3._
 
 import scala.util.Try
 
@@ -17,8 +17,8 @@ trait RestConnector extends Connector {
 
   protected def mkResponse(request: RequestFunction): ErrorOrJsonResponse =
     for {
-      errorOrResponse <- Try(request(basicRequest).body.toString).toEither.leftMap(ex => s"Cannot send request! ${ex.getWithStackTrace}")
-      response <- Right(errorOrResponse)
+      errorOrResponse <- Try(backend.send(request(basicRequest)).body).toEither.leftMap(ex => s"Cannot send request! ${ex.getWithStackTrace}")
+      response <- errorOrResponse
     } yield Json.parse(response)
 
   override def close(): Unit = backend.close()

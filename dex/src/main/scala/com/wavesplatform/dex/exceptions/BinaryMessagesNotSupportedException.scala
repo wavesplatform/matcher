e7284b6894a7c79c88d404e04758d5e937e34a82
nik <ngadiyak@gmail.com>
2021-03-18 14:49:02 +0300

    DEX-1113-BinaryMessagesNotSupportedException

--- dex/src/main/scala/com/wavesplatform/dex/exceptions/BinaryMessagesNotSupportedException.scala
+++ dex/src/main/scala/com/wavesplatform/dex/exceptions/BinaryMessagesNotSupportedException.scala
@@ -2,4 +2,4 @@ package com.wavesplatform.dex.exceptions
 
 import scala.util.control.NoStackTrace
 
-final case class BinaryMessagesNotSupportedException(message: String, cause: Throwable) extends Exception(message, cause) with NoStackTrace
+final case class BinaryMessagesNotSupportedException(message: String) extends Exception(message) with NoStackTrace

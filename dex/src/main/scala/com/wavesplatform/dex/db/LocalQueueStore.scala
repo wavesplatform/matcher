6625fa42de2d6848e28b0cd085c461ca12b9c1ff
Evgenia Tsybrenko <elle4holloway@gmail.com>
2021-04-07 15:16:29 +0600

    DEX-1076 made async LocalQueueStore

--- dex/src/main/scala/com/wavesplatform/dex/db/LocalQueueStore.scala
+++ dex/src/main/scala/com/wavesplatform/dex/db/LocalQueueStore.scala
@@ -1,38 +1,56 @@
 package com.wavesplatform.dex.db
 
+import cats.Monad
+import cats.implicits._
+
 import java.util.concurrent.ConcurrentLinkedQueue
 import java.util.concurrent.atomic.AtomicLong
-
 import com.google.common.primitives.{Longs, Shorts}
 import com.wavesplatform.dex.db.DbKeys._
-import com.wavesplatform.dex.db.leveldb.{DBExt, ReadOnlyDB}
+import com.wavesplatform.dex.db.leveldb.Key
 import com.wavesplatform.dex.queue.{ValidatedCommand, ValidatedCommandWithMeta}
-import org.iq80.leveldb.{DB, ReadOptions}
+import com.wavesplatform.dex.db.leveldb.LevelDb
 
-class LocalQueueStore(db: DB) {
+trait AbstractLocalQueueStore[F[_]] {
 
-  private val newestIdx = new AtomicLong(db.get(lqNewestIdx))
+  def enqueue(command: ValidatedCommand, timestamp: Long): F[ValidatedCommandWithMeta.Offset]
+
+  def getFrom(offset: ValidatedCommandWithMeta.Offset, maxElements: Int): F[Vector[ValidatedCommandWithMeta]]
+
+  def oldestOffset: F[Option[ValidatedCommandWithMeta.Offset]]
+
+  def newestOffset: F[Option[ValidatedCommandWithMeta.Offset]]
+
+  def dropUntil(offset: ValidatedCommandWithMeta.Offset): F[Unit]
+
+}
+
+class LocalQueueStore[F[_]](levelDb: LevelDb[F])(implicit fa: Monad[F]) extends AbstractLocalQueueStore[F]{
+
+  private val newestIdx = levelDb.get(lqNewestIdx).map(s => new AtomicLong(s))
   private val inMemQueue = new ConcurrentLinkedQueue[ValidatedCommandWithMeta]
   private var startInMemOffset = Option.empty[ValidatedCommandWithMeta.Offset]
 
-  def enqueue(command: ValidatedCommand, timestamp: Long): ValidatedCommandWithMeta.Offset = {
-    val offset = newestIdx.incrementAndGet()
-    val eventKey = lpqElement(offset)
+  override def enqueue(command: ValidatedCommand, timestamp: Long): F[ValidatedCommandWithMeta.Offset] = {
+    newestIdx.flatMap { oldOffset =>
+      val offset = oldOffset.incrementAndGet()
+      val eventKey: Key[Option[ValidatedCommandWithMeta]] = lpqElement(offset)
 
-    val x = ValidatedCommandWithMeta(offset, timestamp, command)
-    db.readWrite { rw =>
-      rw.put(eventKey, Some(x))
-      rw.put(lqNewestIdx, offset)
+      val x = ValidatedCommandWithMeta(offset, timestamp, command)
+      levelDb.readWrite { rw =>
+        rw.put(eventKey, Some(x))
+        rw.put(lqNewestIdx, offset)
+      }.map { _ =>
+        inMemQueue.add(x)
+        if (startInMemOffset.isEmpty) startInMemOffset = Some(offset)
+        offset
+      }
     }
-
-    inMemQueue.add(x)
-    if (startInMemOffset.isEmpty) startInMemOffset = Some(offset)
-    offset
   }
 
-  def getFrom(offset: ValidatedCommandWithMeta.Offset, maxElements: Int): Vector[ValidatedCommandWithMeta] =
+  override def getFrom(offset: ValidatedCommandWithMeta.Offset, maxElements: Int): F[Vector[ValidatedCommandWithMeta]] =
     if (startInMemOffset.exists(_ <= offset))
-      if (inMemQueue.isEmpty) Vector.empty
+      if (inMemQueue.isEmpty) Vector.empty[ValidatedCommandWithMeta].pure[F]
       else {
         val xs = Vector.newBuilder[ValidatedCommandWithMeta]
         var added = 0
@@ -43,33 +61,38 @@ class LocalQueueStore(db: DB) {
         }
 
         xs.result()
-      }
+      }.pure[F]
     else
-      new ReadOnlyDB(db, new ReadOptions())
-        .read(LqElementKeyName, LqElementPrefixBytes, lpqElement(math.max(offset, 0)).keyBytes, Int.MaxValue) { e =>
+      levelDb.readOnly { rw =>
+        rw.read(LqElementKeyName, LqElementPrefixBytes, lpqElement(math.max(offset, 0)).keyBytes, Int.MaxValue) { e =>
           val offset = Longs.fromByteArray(e.getKey.slice(Shorts.BYTES, Shorts.BYTES + Longs.BYTES))
           lpqElement(offset).parse(e.getValue).getOrElse(throw new RuntimeException(s"Can't find a queue event at $offset"))
         }
-
-  def oldestOffset: Option[ValidatedCommandWithMeta.Offset] =
-    new ReadOnlyDB(db, new ReadOptions())
-      .read(LqElementKeyName, LqElementPrefixBytes, lpqElement(0).keyBytes, 1) { e =>
-        Longs.fromByteArray(e.getKey.slice(Shorts.BYTES, Shorts.BYTES + Longs.BYTES))
       }
-      .headOption
 
-  def newestOffset: Option[ValidatedCommandWithMeta.Offset] = {
-    val idx = newestIdx.get()
-    val eventKey = lpqElement(idx)
-    eventKey.parse(db.get(eventKey.keyBytes)).map(_.offset)
+  override def oldestOffset: F[Option[ValidatedCommandWithMeta.Offset]] =
+    levelDb.readOnly { rw =>
+      rw.read(LqElementKeyName, LqElementPrefixBytes, lpqElement(0).keyBytes, 1) { e =>
+        Longs.fromByteArray(e.getKey.slice(Shorts.BYTES, Shorts.BYTES + Longs.BYTES))
+      }.headOption
+    }
+
+  override def newestOffset: F[Option[ValidatedCommandWithMeta.Offset]] = {
+    newestIdx.flatMap { oldOffset =>
+      val idx = oldOffset.get()
+      val eventKey = lpqElement(idx)
+      levelDb.get(eventKey).map(_.map(_.offset))
+    }
   }
 
-  def dropUntil(offset: ValidatedCommandWithMeta.Offset): Unit = db.readWrite { rw =>
-    val oldestIdx = math.max(db.get(lqOldestIdx), 0)
-    (oldestIdx until offset).foreach { offset =>
-      rw.delete(lpqElement(offset))
+  override def dropUntil(offset: ValidatedCommandWithMeta.Offset): F[Unit] = levelDb.get(lqOldestIdx).flatMap { idx =>
+    levelDb.readWrite { rw =>
+      val oldestIdx = math.max(idx, 0)
+      (oldestIdx until offset).foreach { offset =>
+        rw.delete(lpqElement(offset))
+      }
+      rw.put(lqOldestIdx, offset)
     }
-    rw.put(lqOldestIdx, offset)
   }
 
 }

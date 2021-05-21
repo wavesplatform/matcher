574e90e40874fab9107e15df5aba62ff0b25d85f
alexander-branevskiy <alexander-branevskiy@gmail.com>
2021-05-22 01:00:25 +0300

    DEX-1152 remove path

--- dex/src/main/scala/com/wavesplatform/dex/api/http/directives/HttpKamonMetricsDirectives.scala
+++ dex/src/main/scala/com/wavesplatform/dex/api/http/directives/HttpKamonMetricsDirectives.scala
@@ -4,8 +4,7 @@ import akka.http.scaladsl.model.AttributeKey
 import akka.http.scaladsl.server.Directives.{extractRequest, mapResponse}
 import kamon.Kamon
 import kamon.metric.{Counter, Timer}
-
-import java.util.concurrent.{ScheduledThreadPoolExecutor, TimeUnit}
+import kamon.tag.TagSet
 
 object HttpKamonMetricsDirectives {
 
@@ -14,23 +13,16 @@ object HttpKamonMetricsDirectives {
   private val timer = Kamon.timer("matcher.http.endpoints.timer")
   private val counter = Kamon.counter("matcher.http.responses.counter")
 
-  val ec = new ScheduledThreadPoolExecutor(1)
-
   def measureResponse(endpoint: String) = extractRequest.tflatMap { req =>
-    val path = req._1.uri.path.toString()
+//    val path = req._1.uri.path.toString()
     val method = req._1.method.value
-    val startedTimer = timer.withTag("path", path).withTag("endpoint", endpoint).withTag("method", method)
-    val taggedCounter = counter.withTag("path", path).withTag("endpoint", endpoint).withTag("method", method)
-    ec.schedule(
-      new Runnable {
-        override def run(): Unit = {
-          startedTimer.asInstanceOf[kamon.metric.Instrument[_, _]].remove()
-          taggedCounter.asInstanceOf[kamon.metric.Instrument[_, _]].remove()
-        }
-      },
-      60,
-      TimeUnit.SECONDS
-    )
+    val tagset =
+      TagSet.from(Map(
+        "endpoint" -> endpoint,
+        "method" -> method
+      ))
+    val startedTimer = timer.withTags(tagset)
+    val taggedCounter = counter.withTags(tagset)
     val metrics = RequestMetrics(taggedCounter, startedTimer.start())
     mapResponse(_.addAttribute(requestMetricsAttributeKey, metrics))
   }

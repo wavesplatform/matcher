fa7e3f7c3ba745dcc286cc92aab96508a2c59b7b
Evgeniya Tsybrenko <elle4holloway@gmail.com>
2021-05-24 20:23:55 +0600

    DEX-1180 improved test

--- dex-it/src/test/scala/com/wavesplatform/it/sync/compat/DatabaseBackwardCompatTestSuite.scala
+++ dex-it/src/test/scala/com/wavesplatform/it/sync/compat/DatabaseBackwardCompatTestSuite.scala
@@ -40,16 +40,15 @@ class DatabaseBackwardCompatTestSuite extends BackwardCompatSuiteBase {
       orders.size shouldBe > (100) // if its > 100, then there will be not only limit or place orders
     }
 
-    val notFilledOrder = orders.groupBy(_.assetPair).valuesIterator.map { orders =>
-      val order = orders.last
-      val status = dex2.api.waitForOrder(order)(_.status != Status.NotFound)
-      (order, status)
-    }.find(_._2.status == Status.Accepted).map(_._1)
+    orders.groupBy(_.assetPair).valuesIterator.foreach { orders =>
+      dex2.api.waitForOrder(orders.last)(_.status != Status.NotFound)
+    }
 
     markup("cancel one order at DEX2")
-    val orderToCancel = notFilledOrder.last
-    dex2.api.cancelOrderById(orderToCancel)
-    dex2.api.waitForOrderStatus(orderToCancel, Status.Cancelled)
+    val owner = Gen.oneOf(List(alice, bob)).sample.get
+    val orderToCancel = dex1.api.getOrderHistoryByPublicKey(owner, activeOnly = Some(true)).head
+    dex2.api.cancelOrderById(orderToCancel.id, Some(owner.publicKey))
+    dex2.api.waitForOrderStatus(orderToCancel.assetPair, orderToCancel.id, Status.Cancelled)
 
     markup("place order for additional asset pair at DEX2")
     val ts = System.currentTimeMillis()
@@ -81,7 +80,7 @@ class DatabaseBackwardCompatTestSuite extends BackwardCompatSuiteBase {
     orders.groupBy(_.assetPair).valuesIterator.foreach { orders =>
       dex1.api.waitForOrder(orders.last)(_.status != Status.NotFound)
     }
-    dex1.api.waitForOrderStatus(orderToCancel, Status.Cancelled)
+    dex1.api.waitForOrderStatus(orderToCancel.assetPair, orderToCancel.id, Status.Cancelled)
     Thread.sleep(2.minutes.toMillis) // An additional time to wait the concurrent processing
 
     markup("check DEX1 and DEX2 states")

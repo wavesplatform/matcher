628261da1ea4cc412cd2f2f668048043dc201e03
Gadiyak Nikita <ngadiyak@web3tech.ru>
2021-05-27 17:23:15 +0300

    fix

--- dex-ws-load/src/test/scala/load/ConnectionsSimulation.scala
+++ dex-ws-load/src/test/scala/load/ConnectionsSimulation.scala
@@ -6,7 +6,7 @@ class ConnectionsSimulation extends Simulation {
 
   val feeder = separatedValues(System.getProperty("ff"), ';').batch
 
-  val scn = scenario("ConnectionsSimulation")
+  val connectionsSimulation = scenario("ConnectionsSimulation")
     .feed(feeder)
     .exec(connect)
     .repeat(System.getProperty("rt").toInt) {
@@ -14,7 +14,29 @@ class ConnectionsSimulation extends Simulation {
     }
     .exec(close)
 
+  val streamsSimulation = scenario("StreamsSimulation")
+    .feed(feeder)
+    .exec(connect)
+    .exec(ws("WS -- AU").sendText("""${m}"""))
+    .exec(ws("WS -- OB").sendText("""${o0}"""))
+    .exec(ws("WS -- OB").sendText("""${o1}"""))
+    .exec(ws("WS -- OB").sendText("""${o2}"""))
+    .exec(ws("WS -- OB").sendText("""${o3}"""))
+    .exec(ws("WS -- OB").sendText("""${o4}"""))
+    .exec(ws("WS -- OB").sendText("""${o5}"""))
+    .exec(ws("WS -- OB").sendText("""${o6}"""))
+    .exec(ws("WS -- OB").sendText("""${o7}"""))
+    .exec(ws("WS -- OB").sendText("""${o8}"""))
+    .exec(ws("WS -- OB").sendText("""${o9}"""))
+    .repeat(System.getProperty("rt").toInt) {
+      pingPong
+    }
+    .exec(close)
+
   setUp(
-    scn.inject(atOnceUsers(System.getProperty("uc").toInt))
+    System.getProperty("sm") match {
+      case "cs" =>  streamsSimulation.inject(atOnceUsers(System.getProperty("uc").toInt))
+      case "co" =>  connectionsSimulation.inject(atOnceUsers(System.getProperty("uc").toInt))
+    }
   )
 }

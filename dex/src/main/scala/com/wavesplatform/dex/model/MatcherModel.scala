bd0cfb5dae9324de98ce24ac6fca597ef3673ffd
alexander-branevskiy <alexander-branevskiy@gmail.com>
2021-06-02 17:55:31 +0300

    DEX-1190 fix order cancel failed

--- dex/src/main/scala/com/wavesplatform/dex/model/MatcherModel.scala
+++ dex/src/main/scala/com/wavesplatform/dex/model/MatcherModel.scala
@@ -500,7 +500,7 @@ object Events {
 
   case class OrderCancelFailed(id: Order.Id, reason: error.MatcherError)
 
-  case class OrderCancelFailedFinalized(order: Order, reason: error.MatcherError)
+  case class OrderCancelFailedFinalized(orderCancelFailed: OrderCancelFailed, owner: Address)
 
   case class ExchangeTransactionCreated(tx: ExchangeTransaction)
 }

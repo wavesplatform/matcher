bde6004d1179a5029b35f948f2a18fda8d21e311
abranevskiy <alexander.branevskiy@gmail.com>
2021-06-05 17:57:15 +0300

    DEX-1149 remove comment

--- dex/src/main/scala/com/wavesplatform/dex/effect/package.scala
+++ dex/src/main/scala/com/wavesplatform/dex/effect/package.scala
@@ -11,10 +11,7 @@ package object effect {
 
   type FutureResult[T] = EitherT[Future, MatcherError, T]
 
-  //we need this "def" due to Kamon caching futures problem
-  //https://github.com/kamon-io/Kamon/issues/829
   def successAsync: FutureResult[Unit] = liftValueAsync(())
-
   def liftValueAsync[T](value: T): FutureResult[T] = EitherT(Future.successful(value.asRight[MatcherError]))
   def liftErrorAsync[T](error: MatcherError): FutureResult[T] = EitherT(Future.successful(error.asLeft[T]))
   def liftFutureAsync[T](x: Future[T])(implicit ex: ExecutionContext): FutureResult[T] = EitherT.right[MatcherError](x)

2d782850735a0f3682edad019825f5b9753782b7
Evgeniya Tsybrenko <elle4holloway@gmail.com>
2021-06-10 10:59:01 +0600

    DEX-1187  WaitingOffsetToolSettings is final

--- dex/src/main/scala/com/wavesplatform/dex/settings/WaitingOffsetToolSettings.scala
+++ dex/src/main/scala/com/wavesplatform/dex/settings/WaitingOffsetToolSettings.scala
@@ -2,7 +2,7 @@ package com.wavesplatform.dex.settings
 
 import scala.concurrent.duration._
 
-case class WaitingOffsetToolSettings(
+final case class WaitingOffsetToolSettings(
   maxWaitingTime: FiniteDuration,
   checkInterval: FiniteDuration
 )

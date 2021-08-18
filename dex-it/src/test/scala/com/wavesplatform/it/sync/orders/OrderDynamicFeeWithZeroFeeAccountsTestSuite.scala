b96466853c9b6f9d47674d30d561e8505d1d5508
Alexander Branevskiy <abranevskiy@web3tech.ru>
2021-08-18 15:27:22 +0300

    DEX-1318 review fixes (1)

--- dex-it/src/test/scala/com/wavesplatform/it/sync/orders/OrderDynamicFeeWithZeroFeeAccountsTestSuite.scala
+++ dex-it/src/test/scala/com/wavesplatform/it/sync/orders/OrderDynamicFeeWithZeroFeeAccountsTestSuite.scala
@@ -2,7 +2,6 @@ package com.wavesplatform.it.sync.orders
 
 import com.typesafe.config.{Config, ConfigFactory}
 import com.wavesplatform.dex.api.http.entities.HttpOrderStatus.Status
-import com.wavesplatform.dex.domain.account.PublicKey
 import com.wavesplatform.dex.domain.asset.Asset
 import com.wavesplatform.dex.domain.asset.Asset.{IssuedAsset, Waves}
 import com.wavesplatform.dex.domain.order.{Order, OrderType}
@@ -33,31 +32,30 @@ final class OrderDynamicFeeWithZeroFeeAccountsTestSuite extends MatcherSuiteBase
     }
 
     "should return reserved balance based on order for zero fee accounts" in {
-      restartWithNewZeroFeeAccounts(Set(alice))
-      val aliceOrder = mkAliceOrder(10.waves, 1.usd, OrderType.SELL)
-      placeAndAwaitAtDex(aliceOrder)
-      dex1.api.getReservedBalanceWithApiKey(alice).get(Waves).value shouldBe 10.waves + matcherFee
-      dex1.api.cancelOrderById(aliceOrder)
+      val carolOrder = mkCarolOrder(10.waves, 1.usd, OrderType.SELL)
+      placeAndAwaitAtDex(carolOrder)
+      dex1.api.getReservedBalanceWithApiKey(carol).get(Waves).value shouldBe 10.waves + matcherFee
+      dex1.api.cancelOrderById(carolOrder)
       eventually {
-        dex1.api.getReservedBalanceWithApiKey(alice) shouldBe empty
+        dex1.api.getReservedBalanceWithApiKey(carol) shouldBe empty
       }
     }
 
     "should match two orders with fee in waves" in {
       forAll(Table(
         ("maker", "taker"),
-        (mkAliceOrder(10.waves, 1.usd, OrderType.BUY), mkBobOrder(10.waves, 1.usd, OrderType.SELL)),
-        (mkBobOrder(10.waves, 1.usd, OrderType.SELL), mkAliceOrder(10.waves, 1.usd, OrderType.BUY))
+        (mkCarolOrder(10.waves, 1.usd, OrderType.BUY), mkBobOrder(10.waves, 1.usd, OrderType.SELL)),
+        (mkBobOrder(10.waves, 1.usd, OrderType.SELL), mkCarolOrder(10.waves, 1.usd, OrderType.BUY))
       )) { (makerOrder, takerOrder) =>
         test { ib =>
           placeAndAwaitAtDex(makerOrder)
           placeAndAwaitAtDex(takerOrder, Status.Filled)
 
           eventually {
-            wavesNode1.api.wavesBalance(alice) shouldBe ib.aliceWavesBalance + 10.waves
+            wavesNode1.api.wavesBalance(carol) shouldBe ib.carolWavesBalance + 10.waves
             wavesNode1.api.wavesBalance(bob) shouldBe ib.bobWavesBalance - 10.waves - matcherFee
             val expectedUsd = AcceptedOrder.calcAmountOfPriceAsset(10.waves, 1.usd)
-            wavesNode1.api.balance(alice, usd) shouldBe ib.aliceUsdBalance - expectedUsd
+            wavesNode1.api.balance(carol, usd) shouldBe ib.carolUsdBalance - expectedUsd
             wavesNode1.api.balance(bob, usd) shouldBe ib.bobUsdBalance + expectedUsd
             wavesNode1.api.wavesBalance(matcher) shouldBe ib.matcherWavesBalance + matcherFee - tradeFee
           }
@@ -72,12 +70,12 @@ final class OrderDynamicFeeWithZeroFeeAccountsTestSuite extends MatcherSuiteBase
       forAll(Table(
         ("maker", "taker"),
         (
-          mkAliceOrder(10.waves, 1.usd, OrderType.BUY, feeAsset = usd, matcherFee = usdFee),
+          mkCarolOrder(10.waves, 1.usd, OrderType.BUY, feeAsset = usd, matcherFee = usdFee),
           mkBobOrder(10.waves, 1.usd, OrderType.SELL, feeAsset = usd, matcherFee = usdFee)
         ),
         (
           mkBobOrder(10.waves, 1.usd, OrderType.SELL, feeAsset = usd, matcherFee = usdFee),
-          mkAliceOrder(10.waves, 1.usd, OrderType.BUY, feeAsset = usd, matcherFee = usdFee)
+          mkCarolOrder(10.waves, 1.usd, OrderType.BUY, feeAsset = usd, matcherFee = usdFee)
         )
       )) { (makerOrder, takerOrder) =>
         test { ib =>
@@ -85,10 +83,10 @@ final class OrderDynamicFeeWithZeroFeeAccountsTestSuite extends MatcherSuiteBase
           placeAndAwaitAtDex(takerOrder, Status.Filled)
 
           eventually {
-            wavesNode1.api.wavesBalance(alice) shouldBe ib.aliceWavesBalance + 10.waves
+            wavesNode1.api.wavesBalance(carol) shouldBe ib.carolWavesBalance + 10.waves
             wavesNode1.api.wavesBalance(bob) shouldBe ib.bobWavesBalance - 10.waves
             val expectedUsd = AcceptedOrder.calcAmountOfPriceAsset(10.waves, 1.usd)
-            wavesNode1.api.balance(alice, usd) shouldBe ib.aliceUsdBalance - expectedUsd
+            wavesNode1.api.balance(carol, usd) shouldBe ib.carolUsdBalance - expectedUsd
             wavesNode1.api.balance(bob, usd) shouldBe ib.bobUsdBalance + expectedUsd - usdFee
             wavesNode1.api.wavesBalance(matcher) shouldBe ib.matcherWavesBalance - tradeFee
             wavesNode1.api.balance(matcher, usd) shouldBe ib.matcherUsdBalance + usdFee
@@ -100,13 +98,13 @@ final class OrderDynamicFeeWithZeroFeeAccountsTestSuite extends MatcherSuiteBase
     "should match several orders with large market order placed by zero fee account" in test { ib =>
       List(mkBobOrder(10.waves, 1.usd, OrderType.SELL), mkBobOrder(10.waves, 1.usd, OrderType.SELL))
         .foreach(placeAndAwaitAtDex(_))
-      placeAndAwaitAtDex(mkAliceOrder(20.waves, 1.usd, OrderType.BUY), Status.Filled, isMarketOrder = true)
+      placeAndAwaitAtDex(mkCarolOrder(20.waves, 1.usd, OrderType.BUY), Status.Filled, isMarketOrder = true)
 
       eventually {
-        wavesNode1.api.wavesBalance(alice) shouldBe ib.aliceWavesBalance + 20.waves
+        wavesNode1.api.wavesBalance(carol) shouldBe ib.carolWavesBalance + 20.waves
         wavesNode1.api.wavesBalance(bob) shouldBe ib.bobWavesBalance - 20.waves - 2 * matcherFee
         val expectedUsd = AcceptedOrder.calcAmountOfPriceAsset(20.waves, 1.usd)
-        wavesNode1.api.balance(alice, usd) shouldBe ib.aliceUsdBalance - expectedUsd
+        wavesNode1.api.balance(carol, usd) shouldBe ib.carolUsdBalance - expectedUsd
         wavesNode1.api.balance(bob, usd) shouldBe ib.bobUsdBalance + expectedUsd
         wavesNode1.api.wavesBalance(matcher) shouldBe ib.matcherWavesBalance + 2 * matcherFee - 2 * tradeFee
       }
@@ -116,12 +114,12 @@ final class OrderDynamicFeeWithZeroFeeAccountsTestSuite extends MatcherSuiteBase
       forAll(Table(
         ("maker", "taker"),
         (
-          List(mkAliceOrder(20.waves, 1.usd, OrderType.BUY)),
+          List(mkCarolOrder(20.waves, 1.usd, OrderType.BUY)),
           List(mkBobOrder(10.waves, 1.usd, OrderType.SELL), mkBobOrder(10.waves, 1.usd, OrderType.SELL))
         ),
         (
           List(mkBobOrder(10.waves, 1.usd, OrderType.SELL), mkBobOrder(10.waves, 1.usd, OrderType.SELL)),
-          List(mkAliceOrder(20.waves, 1.usd, OrderType.BUY))
+          List(mkCarolOrder(20.waves, 1.usd, OrderType.BUY))
         )
       )) { (makerOrders, takerOrders) =>
         test { ib =>
@@ -129,10 +127,10 @@ final class OrderDynamicFeeWithZeroFeeAccountsTestSuite extends MatcherSuiteBase
           takerOrders.foreach(placeAndAwaitAtDex(_, Status.Filled))
 
           eventually {
-            wavesNode1.api.wavesBalance(alice) shouldBe ib.aliceWavesBalance + 20.waves
+            wavesNode1.api.wavesBalance(carol) shouldBe ib.carolWavesBalance + 20.waves
             wavesNode1.api.wavesBalance(bob) shouldBe ib.bobWavesBalance - 20.waves - 2 * matcherFee
             val expectedUsd = AcceptedOrder.calcAmountOfPriceAsset(20.waves, 1.usd)
-            wavesNode1.api.balance(alice, usd) shouldBe ib.aliceUsdBalance - expectedUsd
+            wavesNode1.api.balance(carol, usd) shouldBe ib.carolUsdBalance - expectedUsd
             wavesNode1.api.balance(bob, usd) shouldBe ib.bobUsdBalance + expectedUsd
             wavesNode1.api.wavesBalance(matcher) shouldBe ib.matcherWavesBalance + 2 * matcherFee - 2 * tradeFee
           }
@@ -141,11 +139,11 @@ final class OrderDynamicFeeWithZeroFeeAccountsTestSuite extends MatcherSuiteBase
     }
 
     "should take zero fees for zero fee accounts while trading with self" in test { ib =>
-      placeAndAwaitAtDex(mkAliceOrder(20.waves, 1.usd, OrderType.BUY))
-      placeAndAwaitAtDex(mkAliceOrder(20.waves, 1.usd, OrderType.SELL), Status.Filled)
+      placeAndAwaitAtDex(mkCarolOrder(20.waves, 1.usd, OrderType.BUY))
+      placeAndAwaitAtDex(mkCarolOrder(20.waves, 1.usd, OrderType.SELL), Status.Filled)
       eventually {
-        wavesNode1.api.wavesBalance(alice) shouldBe ib.aliceWavesBalance
-        wavesNode1.api.balance(alice, usd) shouldBe ib.aliceUsdBalance
+        wavesNode1.api.wavesBalance(carol) shouldBe ib.carolWavesBalance
+        wavesNode1.api.balance(carol, usd) shouldBe ib.carolUsdBalance
         wavesNode1.api.wavesBalance(matcher) shouldBe ib.matcherWavesBalance - tradeFee
       }
     }
@@ -153,6 +151,9 @@ final class OrderDynamicFeeWithZeroFeeAccountsTestSuite extends MatcherSuiteBase
   }
 
   private val ts = new AtomicLong(System.currentTimeMillis())
+
+  private def mkCarolOrder(amount: Long, price: Long, orderType: OrderType, feeAsset: Asset = Waves, matcherFee: Long = matcherFee): Order =
+    mkOrder(carol, wavesUsdPair, orderType, amount, price, matcherFee, feeAsset, ts.incrementAndGet())
 
   private def mkAliceOrder(amount: Long, price: Long, orderType: OrderType, feeAsset: Asset = Waves, matcherFee: Long = matcherFee): Order =
     mkOrder(alice, wavesUsdPair, orderType, amount, price, matcherFee, feeAsset, ts.incrementAndGet())
@@ -164,35 +165,23 @@ final class OrderDynamicFeeWithZeroFeeAccountsTestSuite extends MatcherSuiteBase
     case (asset, rate) => withClue(s"$asset")(dex1.api.upsertAssetRate(asset, rate))
   }
 
-  private def restartWithNewZeroFeeAccounts(zeroFeeAccounts: Set[PublicKey]): Unit = {
-    val newConfig = ConfigFactory.parseString(
+  override protected def dexInitialSuiteConfig: Config =
+    ConfigFactory.parseString(
       s"""
-         |waves.dex.order-fee.-1.dynamic {
-         |  zero-fee-accounts = [${zeroFeeAccounts.map(x => s""" "${x.base58}" """.trim).mkString(",")}]
+         |waves.dex {
+         |  price-assets = [ "$UsdId", "WAVES" ]
+         |  allowed-order-versions = [1, 2, 3]
+         |  order-fee.-1 {
+         |    mode = dynamic
+         |    dynamic {
+         |      base-maker-fee = $matcherFee
+         |      base-taker-fee = $matcherFee
+         |      zero-fee-accounts = ["${carol.publicKey.base58}"]
+         |    }
+         |  }
          |}
-         |""".stripMargin
-    ).withFallback(dexInitialConf)
-    dex1.restartWithNewSuiteConfig(newConfig)
-  }
-
-  private val dexInitialConf = ConfigFactory.parseString(
-    s"""
-       |waves.dex {
-       |  price-assets = [ "$UsdId", "WAVES" ]
-       |  allowed-order-versions = [1, 2, 3]
-       |  order-fee.-1 {
-       |    mode = dynamic
-       |    dynamic {
-       |      base-maker-fee = $matcherFee
-       |      base-taker-fee = $matcherFee
-       |      zero-fee-accounts = []
-       |    }
-       |  }
-       |}
        """.stripMargin
-  )
-
-  override protected def dexInitialSuiteConfig: Config = dexInitialConf
+    )
 
   private def test[A](f: InitialBalances => A): Unit = {
     val matcherWavesBalance = wavesNode1.api.wavesBalance(matcher)
@@ -201,7 +190,18 @@ final class OrderDynamicFeeWithZeroFeeAccountsTestSuite extends MatcherSuiteBase
     val bobUsdBalance = wavesNode1.api.balance(bob, usd)
     val aliceWavesBalance = wavesNode1.api.wavesBalance(alice)
     val aliceUsdBalance = wavesNode1.api.balance(alice, usd)
-    f(InitialBalances(matcherWavesBalance, matcherUsdBalance, bobWavesBalance, bobUsdBalance, aliceWavesBalance, aliceUsdBalance))
+    val carolWavesBalance = wavesNode1.api.wavesBalance(carol)
+    val carolUsdBalance = wavesNode1.api.balance(carol, usd)
+    f(InitialBalances(
+      matcherWavesBalance,
+      matcherUsdBalance,
+      bobWavesBalance,
+      bobUsdBalance,
+      aliceWavesBalance,
+      aliceUsdBalance,
+      carolWavesBalance,
+      carolUsdBalance
+    ))
   }
 
   override protected def beforeAll(): Unit = {
@@ -209,10 +209,14 @@ final class OrderDynamicFeeWithZeroFeeAccountsTestSuite extends MatcherSuiteBase
     broadcastAndAwait(IssueUsdTx)
     broadcastAndAwait(
       mkTransfer(alice, bob, 500_000.usd, usd),
-      mkTransfer(alice, matcher, 500_000.usd, usd)
+      mkTransfer(alice, matcher, 500_000.usd, usd),
+      mkTransfer(alice, carol, 500_000.usd, usd),
+      mkTransfer(alice, carol, 500_000.waves, Waves)
     )
     dex1.start()
   }
+
+  private lazy val carol = mkKeyPair("carol")
 
   private case class InitialBalances(
     matcherWavesBalance: Long,
@@ -220,7 +224,9 @@ final class OrderDynamicFeeWithZeroFeeAccountsTestSuite extends MatcherSuiteBase
     bobWavesBalance: Long,
     bobUsdBalance: Long,
     aliceWavesBalance: Long,
-    aliceUsdBalance: Long
+    aliceUsdBalance: Long,
+    carolWavesBalance: Long,
+    carolUsdBalance: Long
   )
 
 }

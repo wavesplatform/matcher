e4918c2cec15f1ccbc8a8b3309200d3b439d041c
nik <ngadiyak@gmail.com>
2021-09-07 18:04:54 +0300

    DEX-1379-insta-ws-send-diff

---
+++ dex-it/src/test/scala/com/wavesplatform/it/sync/api/ws/WsAddressStreamRealTimeTestSuite.scala
@@ -0,0 +1,59 @@
+package com.wavesplatform.it.sync.api.ws
+
+import com.typesafe.config.{Config, ConfigFactory}
+import com.wavesplatform.dex.api.ws.connection.WsConnection
+import com.wavesplatform.dex.api.ws.entities.{WsBalances, WsMatchTransactionInfo, WsOrder}
+import com.wavesplatform.dex.domain.account.KeyPair
+import com.wavesplatform.dex.domain.account.KeyPair.toAddress
+import com.wavesplatform.dex.domain.asset.Asset.Waves
+import com.wavesplatform.dex.domain.order.OrderType.{BUY, SELL}
+import com.wavesplatform.dex.model.OrderStatus
+import com.wavesplatform.it.WsSuiteBase
+import org.scalatest.prop.TableDrivenPropertyChecks
+
+import scala.util.Using
+
+class WsAddressStreamRealTimeTestSuite extends WsSuiteBase with TableDrivenPropertyChecks {
+
+  val account = mkKeyPair("Test")
+
+  override protected val dexInitialSuiteConfig: Config = ConfigFactory
+    .parseString(s"""waves.dex {
+                    |  price-assets = [ "$UsdId", "WAVES" ]
+                    |  web-sockets.external-client-handler.subscriptions.max-address-number = 3
+                    |  address-actor.realtime-ws-accounts = [${account.publicKey}]
+                    |}""".stripMargin)
+    .withFallback(jwtPublicKeyConfig)
+
+  override protected def beforeAll(): Unit = {
+    wavesNode1.start()
+    broadcastAndAwait(IssueUsdTx)
+    broadcastAndAwait(mkTransfer(alice, account, 100.waves, Waves), mkTransfer(alice, account, 100.usd, usd))
+    dex1.start()
+  }
+
+  private def mkWsAddressConnection(account: KeyPair): WsConnection = mkWsAddressConnection(account, dex1)
+
+  "Address stream should" - {
+
+    "send correct updates when account added to address-actor.realtime-ws-accounts" in {
+      Using.resource(mkWsAddressConnection(account)) { wsc =>
+
+        broadcastAndAwait(mkTransfer(account, alice, 2.waves, Waves))
+        placeAndAwaitAtDex(mkOrder(alice, wavesUsdPair, BUY, 10.waves, 1.usd))
+
+        placeAndAwaitAtNode(mkOrder(account, wavesUsdPair, SELL, 10.waves, 1.usd))
+
+        eventually {
+          wsc.balanceChanges.squashed should matchTo(
+            Map(
+              usd -> WsBalances(110.0, 0),
+              Waves -> WsBalances(87.996, 0.0)
+            )
+          )
+        }
+      }
+    }
+
+  }
+}

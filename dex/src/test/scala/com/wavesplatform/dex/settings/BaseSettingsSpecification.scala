ba748915b2cce4ba8c3dc4eb4e8914eba1ed2455
- <alxndr.mkd@protonmail.com>
2021-10-21 21:51:50 +0300

    fix settings test

--- dex/src/test/scala/com/wavesplatform/dex/settings/BaseSettingsSpecification.scala
+++ dex/src/test/scala/com/wavesplatform/dex/settings/BaseSettingsSpecification.scala
@@ -274,7 +274,7 @@ class BaseSettingsSpecification extends AnyFlatSpec {
          |  rest-api {
          |    address = "127.0.0.1"
          |    port = 6886
-         |    api-key-hash = ""
+         |    api-key-hashes = []
          |    cors = yes
          |    api-key-different-host = no
          |  }

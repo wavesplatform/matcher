42f0a9e26b14d006b73ce00cc952c23dc1ed839c
Evgeniya Tsybrenko <elle4holloway@gmail.com>
2021-11-16 11:52:29 +0600

    DEX-1494 added trim

--- waves-integration/src/main/scala/com/wavesplatform/dex/domain/utils/ScorexLogging.scala
+++ waves-integration/src/main/scala/com/wavesplatform/dex/domain/utils/ScorexLogging.scala
@@ -6,16 +6,16 @@ import org.slf4j.{Logger, LoggerFactory}
 
 case class LoggerFacade(logger: Logger, prefix: String = "") {
 
-  def trace(message: => String): Unit = if (logger.isTraceEnabled) logger.trace(s"{} $message", prefix)
-  def debug(message: => String, arg: Any): Unit = if (logger.isDebugEnabled) logger.debug(s"{} $message", prefix, arg)
-  def debug(message: => String): Unit = if (logger.isDebugEnabled) logger.debug(s"{} $message", prefix)
-  def info(message: => String): Unit = if (logger.isInfoEnabled) logger.info(s"{} $message", prefix)
-  def info(message: => String, arg: Any): Unit = if (logger.isInfoEnabled) logger.info(s"{} $message", prefix, arg)
-  def info(message: => String, throwable: Throwable): Unit = if (logger.isInfoEnabled) logger.info(s"$prefix $message", throwable)
-  def warn(message: => String): Unit = if (logger.isWarnEnabled) logger.warn(s"{} $message", prefix)
-  def warn(message: => String, throwable: Throwable): Unit = if (logger.isWarnEnabled) logger.warn(s"$prefix $message", throwable)
-  def error(message: => String): Unit = if (logger.isErrorEnabled) logger.error(s"{} $message", prefix)
-  def error(message: => String, throwable: Throwable): Unit = if (logger.isErrorEnabled) logger.error(s"$prefix $message", throwable)
+  def trace(message: => String): Unit = if (logger.isTraceEnabled) logger.trace(s"{} $message".trim, prefix)
+  def debug(message: => String, arg: Any): Unit = if (logger.isDebugEnabled) logger.debug(s"{} $message".trim, prefix, arg)
+  def debug(message: => String): Unit = if (logger.isDebugEnabled) logger.debug(s"{} $message".trim, prefix)
+  def info(message: => String): Unit = if (logger.isInfoEnabled) logger.info(s"{} $message".trim, prefix)
+  def info(message: => String, arg: Any): Unit = if (logger.isInfoEnabled) logger.info(s"{} $message".trim, prefix, arg)
+  def info(message: => String, throwable: Throwable): Unit = if (logger.isInfoEnabled) logger.info(s"$prefix $message".trim, throwable)
+  def warn(message: => String): Unit = if (logger.isWarnEnabled) logger.warn(s"{} $message".trim, prefix)
+  def warn(message: => String, throwable: Throwable): Unit = if (logger.isWarnEnabled) logger.warn(s"$prefix $message".trim, throwable)
+  def error(message: => String): Unit = if (logger.isErrorEnabled) logger.error(s"{} $message".trim, prefix)
+  def error(message: => String, throwable: Throwable): Unit = if (logger.isErrorEnabled) logger.error(s"$prefix $message".trim, throwable)
 }
 
 trait ScorexLogging {

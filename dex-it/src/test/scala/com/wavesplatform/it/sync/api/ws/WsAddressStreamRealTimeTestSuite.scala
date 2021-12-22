f45740f9f35f7e3b1a12b1c0504fa3e641cedad2
JennaWestenra <78524706+JennaWestenra@users.noreply.github.com>
2021-12-22 20:58:49 +0600

    DEX-1508 fixed command ordering for AddressActor (#673)
    * DEX-1508 fixed command ordering for AddressActor
    
    * DEX-1508 Seq -> Vector
    
    * DEX-1508 little fixes
    
    * DEX-1508 little fixes
    
    * DEX-1508 fixed test
    
    * DEX-1508 fixed test
    
    * DEX-1508 fixed test
    
    * DEX-1508 fixed test
    
    Co-authored-by: Evgeniya Tsybrenko <elle4holloway@gmail.com>

--- dex-it/src/test/scala/com/wavesplatform/it/sync/api/ws/WsAddressStreamRealTimeTestSuite.scala
+++ dex-it/src/test/scala/com/wavesplatform/it/sync/api/ws/WsAddressStreamRealTimeTestSuite.scala
@@ -55,8 +55,9 @@ class WsAddressStreamRealTimeTestSuite extends WsSuiteBase {
     "send only one update for multiple matches for one order" in
     Using.resource(mkWsAddressConnection(account, dex1)) { wsc =>
 
+      val ts = System.currentTimeMillis()
       val smallOrders = (1 to 5).map { i =>
-        mkOrder(alice, wavesUsdPair, OrderType.SELL, 2.waves, (1 + (i / 10)).usd, ts = System.currentTimeMillis() + (i * 200))
+        mkOrder(alice, wavesUsdPair, OrderType.SELL, 2.waves, (1 + (i / 10)).usd, ts = ts + (i * 200))
       }
       smallOrders.foreach(dex1.api.place)
 
@@ -69,7 +70,7 @@ class WsAddressStreamRealTimeTestSuite extends WsSuiteBase {
 
       val updates = wsc.orderChanges.filter(_.id == bigOrder.id())
 
-      updates.size shouldBe 2 // one for creating and one for five matches
+      updates.size should (be > 0 and be <= 2) // one for creating and one for five matches, or just one update
       updates.last.matchInfo.size shouldBe 5
     }
 

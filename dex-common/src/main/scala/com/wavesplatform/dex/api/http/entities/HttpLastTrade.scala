70ade39e327e70418f8b53f99ad558cfcb31a4a8
Evgeniya Tsybrenko <elle4holloway@gmail.com>
2022-01-11 16:16:35 +0600

    DEX-994 fixed toString

--- dex-common/src/main/scala/com/wavesplatform/dex/api/http/entities/HttpLastTrade.scala
+++ dex-common/src/main/scala/com/wavesplatform/dex/api/http/entities/HttpLastTrade.scala
@@ -2,4 +2,6 @@ package com.wavesplatform.dex.api.http.entities
 
 import com.wavesplatform.dex.domain.order.OrderType
 
-final case class HttpLastTrade(price: Long, amount: Long, side: OrderType)
+final case class HttpLastTrade(price: Long, amount: Long, side: OrderType) {
+  override def toString: String = s"LastTrade(p=$price, a=$amount, $side)"
+}

baa74892619a7befad346024781cab7caf551f56
Alexander Branevskiy <abranevskiy@web3tech.ru>
2022-01-20 20:18:33 +0300

    DEX-1529 fix MatcherTestSuite

--- dex-it/src/test/scala/com/wavesplatform/it/sync/MatcherTestSuite.scala
+++ dex-it/src/test/scala/com/wavesplatform/it/sync/MatcherTestSuite.scala
@@ -43,6 +43,22 @@ class MatcherTestSuite extends MatcherSuiteBase with TableDrivenPropertyChecks {
     s"""waves.dex {
        |  price-assets = [ "$UsdnId", "$BtcId", "$UsdId", "WAVES", $EthId ]
        |  order-db.max-orders = $maxOrders
+       |  order-fee.-1 {
+       |    mode = composite
+       |    composite {
+       |      default {
+       |        mode = dynamic
+       |        dynamic {
+       |          base-maker-fee = ${0.003.waves}
+       |          base-taker-fee = ${0.003.waves}
+       |        }
+       |      }
+       |      discount {
+       |        asset = "$UsdnId"
+       |        value = 0
+       |      }
+       |    }
+       |  }
        |}""".stripMargin
   )
 

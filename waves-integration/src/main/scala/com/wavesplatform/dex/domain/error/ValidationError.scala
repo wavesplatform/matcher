3d772a2558cc81cc4272e88ed5bc445fbe3c8acf
Alexander Branevskiy <abranevskiy@web3tech.ru>
2022-02-12 00:35:51 +0300

    DEX-1546 minor clean up

--- waves-integration/src/main/scala/com/wavesplatform/dex/domain/error/ValidationError.scala
+++ waves-integration/src/main/scala/com/wavesplatform/dex/domain/error/ValidationError.scala
@@ -5,7 +5,7 @@ import com.wavesplatform.dex.domain.order.Order
 import scala.util.Either
 
 trait ValidationError extends Product with Serializable {
-  def message: String
+  val message: String
 }
 
 object ValidationError {
@@ -13,39 +13,39 @@ object ValidationError {
   type Validation[T] = Either[ValidationError, T]
 
   case class InvalidAsset(asset: String, reason: String) extends ValidationError {
-    override def message: String = s"Invalid asset $asset, reason $reason"
+    override val message: String = s"Invalid asset $asset, reason $reason"
   }
 
   case class InvalidBase58String(reason: String) extends ValidationError {
-    override def message: String = s"Invalid base 58 string $reason"
+    override val message: String = s"Invalid base 58 string $reason"
   }
 
   case class InvalidAddress(reason: String) extends ValidationError {
-    override def message: String = s"Invalid address $reason"
+    override val message: String = s"Invalid address $reason"
   }
 
   case class InvalidPublicKey(reason: String) extends ValidationError {
-    override def message: String = s"Invalid public key $reason"
+    override val message: String = s"Invalid public key $reason"
   }
 
   case class NonPositiveAmount(amount: Long, of: String) extends ValidationError {
-    override def message: String = s"Non positive amount $amount of $of"
+    override val message: String = s"Non positive amount $amount of $of"
   }
 
   case class InsufficientFee(msg: String = "insufficient fee") extends ValidationError {
-    override def message: String = s"Insufficient fee $msg"
+    override val message: String = s"Insufficient fee $msg"
   }
 
   case class OrderValidationError(order: Order, err: String) extends ValidationError {
-    override def message: String = s"Order validation error for order $order, err $err"
+    override val message: String = s"Order validation error for order $order, err $err"
   }
 
   case class ActivationError(err: String) extends ValidationError {
-    override def message: String = s"Activation error $err"
+    override val message: String = s"Activation error $err"
   }
 
   case class GenericError(err: String) extends ValidationError {
-    override def message: String = s"Generic error $err"
+    override val message: String = s"Generic error $err"
   }
 
   object GenericError {

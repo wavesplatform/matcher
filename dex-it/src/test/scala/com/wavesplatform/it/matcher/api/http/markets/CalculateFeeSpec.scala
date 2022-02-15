03100a88c1967d2155b5a83b464e0d565eaac6b9
Alexander Branevskiy <abranevskiy@web3tech.ru>
2022-02-15 23:15:42 +0300

    DEX-1555 more tests

--- dex-it/src/test/scala/com/wavesplatform/it/matcher/api/http/markets/CalculateFeeSpec.scala
+++ dex-it/src/test/scala/com/wavesplatform/it/matcher/api/http/markets/CalculateFeeSpec.scala
@@ -110,7 +110,6 @@ final class CalculateFeeSpec extends MatcherSuiteBase with RawHttpChecks {
       )
 
       dex1.api.deleteAssetRate(btc)
-      dex1.api.deleteAssetRate(usdn)
       dex1.api.upsertAssetRate(btc, 8.155223)
       dex1.api.upsertAssetRate(usdn, 20.08102)
 

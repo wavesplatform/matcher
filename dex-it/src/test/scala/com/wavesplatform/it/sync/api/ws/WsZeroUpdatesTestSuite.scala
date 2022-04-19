ebff8531f444606da6cb532bc53c66d5ca1332f2
Evgeniya Tsybrenko <elle4holloway@gmail.com>
2022-04-19 16:52:40 +0600

    DEX-849 fixed test

--- dex-it/src/test/scala/com/wavesplatform/it/sync/api/ws/WsZeroUpdatesTestSuite.scala
+++ dex-it/src/test/scala/com/wavesplatform/it/sync/api/ws/WsZeroUpdatesTestSuite.scala
@@ -1,8 +1,9 @@
 package com.wavesplatform.it.sync.api.ws
 
 import com.typesafe.config.{Config, ConfigFactory}
-import com.wavesplatform.dex.api.ws.entities.WsLastTrade
+import com.wavesplatform.dex.api.ws.entities.{WsBalances, WsLastTrade}
 import com.wavesplatform.dex.api.ws.protocol.{WsAddressChanges, WsOrderBookChanges, WsOrderBookSubscribe}
+import com.wavesplatform.dex.domain.asset.Asset
 import com.wavesplatform.dex.domain.asset.Asset.Waves
 import com.wavesplatform.dex.domain.order.OrderType
 import com.wavesplatform.it.WsSuiteBase
@@ -14,7 +15,7 @@ class WsZeroUpdatesTestSuite extends WsSuiteBase {
   override protected val dexInitialSuiteConfig: Config = ConfigFactory.parseString(
     s"""waves.dex {
        |  price-assets = [ "$UsdnId", "WAVES" ]
-       |  web-sockets.external-client-handler.messages-interval = 3s
+       |  web-sockets.external-client-handler.messages-interval = 2s
        |}""".stripMargin
   ).withFallback(jwtPublicKeyConfig)
 
@@ -33,8 +34,8 @@ class WsZeroUpdatesTestSuite extends WsSuiteBase {
             1
           )
         )
-        eventually(wsc.receiveAtLeastN[WsOrderBookChanges](1))
-        eventually(wsc.receiveAtLeastN[WsAddressChanges](1))
+        val initStateOb = eventually(wsc.receiveAtLeastN[WsOrderBookChanges](1)).head
+        val initStateAc = eventually(wsc.receiveAtLeastN[WsAddressChanges](1)).head
 
         wsc.clearMessages()
 
@@ -44,17 +45,27 @@ class WsZeroUpdatesTestSuite extends WsSuiteBase {
         dex1.api.place(order2)
         waitForOrderAtNode(order2)
 
-        val wacs = wsc.receiveAtLeastN[WsAddressChanges](1)
-        val wobcs = wsc.receiveAtLeastN[WsOrderBookChanges](1)
-        wacs.foreach { wac =>
-          wac.balances.keySet shouldBe Set(Waves)
-          wac.balances(Waves).tradable shouldBe (4_949_950d - 0.003 * 2 - 1) // two orders + issue fee 1.waves
+        wsc.receiveAtLeastN[WsOrderBookChanges](1)
+        wsc.receiveAtLeastN[WsAddressChanges](1)
+        val balances = wsc.addressStateChanges.foldLeft(Map.empty[Asset, WsBalances]) {
+          case (acc, change) =>
+            (acc ++ change.balances).filter {
+              case (asset, balance) => !initStateAc.balances.get(asset).contains(balance)
+            }
         }
-        wobcs.foreach { wobc =>
-          wobc.asks.isEmpty shouldBe true
-          wobc.bids.isEmpty shouldBe true
-          wobc.lastTrade.value shouldBe WsLastTrade(20.0, 50.0, OrderType.SELL)
+        val obChanges = wsc.collectMessages[WsOrderBookChanges].foldLeft(WsOrderBookChanges.empty(wavesUsdnPair)) {
+          case (acc, change) =>
+            acc.copy(
+              asks = change.asks.filter(lvl => lvl._2 != 0),
+              bids = change.bids.filter(lvl => lvl._2 != 0),
+              lastTrade = acc.lastTrade.orElse(change.lastTrade)
+            )
         }
+        balances.keySet shouldBe Set(Waves)
+        balances(Waves).tradable shouldBe (4_949_950d - 0.003 * 2 - 1) // two orders + issue fee 1.waves
+        obChanges.asks.isEmpty shouldBe true
+        obChanges.bids.isEmpty shouldBe true
+        obChanges.lastTrade.value shouldBe WsLastTrade(20.0, 50.0, OrderType.SELL)
         wsc.receiveNoMessages()
       }
     }

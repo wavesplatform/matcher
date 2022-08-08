1850463f15107d60b3c59e966d4c676df75f92a7
alexander-branevskiy <alexander-branevskiy@gmail.com>
2022-08-08 19:09:40 +0600

    DEX-1669 refactors

--- waves-integration/src/main/scala/com/wavesplatform/dex/grpc/integration/protobuf/DexToPbConversions.scala
+++ waves-integration/src/main/scala/com/wavesplatform/dex/grpc/integration/protobuf/DexToPbConversions.scala
@@ -1,11 +1,11 @@
 package com.wavesplatform.dex.grpc.integration.protobuf
 
 import com.google.protobuf.{UnsafeByteOperations, ByteString => PbByteString}
-import com.wavesplatform.dex.domain.account.{Address => VAddress, AddressScheme => VAddressScheme}
-import com.wavesplatform.dex.domain.asset.{Asset => VAsset}
-import com.wavesplatform.dex.domain.bytes.{ByteStr => VByteStr}
-import com.wavesplatform.dex.domain.order.{OrderAuthentication, Order => VOrder, OrderType => VOrderType}
-import com.wavesplatform.dex.domain.transaction.{ExchangeTransaction => VExchangeTransaction}
+import com.wavesplatform.dex.domain.account.{Address => DexAddress, AddressScheme => DexAddressScheme}
+import com.wavesplatform.dex.domain.asset.{Asset => DexAsset}
+import com.wavesplatform.dex.domain.bytes.{ByteStr => DexByteStr}
+import com.wavesplatform.dex.domain.order.{OrderAuthentication => DexOrderAuthentication, Order => DexOrder, OrderType => DexOrderType}
+import com.wavesplatform.dex.domain.transaction.{ExchangeTransaction => DexExchangeTransaction}
 import com.wavesplatform.dex.grpc.integration.services.{ExchangeTransaction => PbDexExchangeTransaction, SignedExchangeTransaction => PbDexSignedExchangeTransaction}
 import com.wavesplatform.protobuf.order.{AssetPair => PbAssetPair, Order => PbOrder}
 import com.wavesplatform.protobuf.transaction.{ExchangeTransactionData => PbExchangeTransactionData}
@@ -15,13 +15,13 @@ import com.wavesplatform.protobuf.transaction.{Transaction => PbTransaction}
 
 object DexToPbConversions {
 
-  implicit final class VanillaExchangeTransactionOps(val tx: VExchangeTransaction) extends AnyVal {
+  implicit final class VanillaExchangeTransactionOps(val tx: DexExchangeTransaction) extends AnyVal {
 
     def toPB: PbDexSignedExchangeTransaction =
       PbDexSignedExchangeTransaction(
         transaction = Some(
           PbDexExchangeTransaction(
-            chainId = tx.chainByte.getOrElse(VAddressScheme.current.chainId).toInt,
+            chainId = tx.chainByte.getOrElse(DexAddressScheme.current.chainId).toInt,
             senderPublicKey = tx.sender.toPB,
             fee = Some(PbAmount(assetId = tx.assetFee._1.toPB, amount = tx.assetFee._2)),
             timestamp = tx.timestamp,
@@ -37,7 +37,7 @@ object DexToPbConversions {
         transaction =
           PbSignedTransaction.Transaction.WavesTransaction(
             PbTransaction(
-              chainId = tx.chainByte.getOrElse(VAddressScheme.current.chainId).toInt,
+              chainId = tx.chainByte.getOrElse(DexAddressScheme.current.chainId).toInt,
               senderPublicKey = tx.sender.toPB,
               fee = Some(PbAmount(assetId = tx.assetFee._1.toPB, amount = tx.assetFee._2)),
               timestamp = tx.timestamp,
@@ -48,7 +48,7 @@ object DexToPbConversions {
         proofs = tx.proofs.proofs.map(_.toPB)
       )
 
-    private def mkPbExchangeTxData(tx: VExchangeTransaction) =
+    private def mkPbExchangeTxData(tx: DexExchangeTransaction) =
       PbExchangeTransactionData(
         amount = tx.amount,
         price = tx.price,
@@ -59,28 +59,28 @@ object DexToPbConversions {
 
   }
 
-  implicit final class VanillaAssetOps(val self: VAsset) extends AnyVal {
+  implicit final class VanillaAssetOps(val self: DexAsset) extends AnyVal {
     def toPB: PbByteString = self.fold(PbByteString.EMPTY)(_.id.toPB)
   }
 
-  implicit final class VanillaAddressOps(val self: VAddress) extends AnyVal {
+  implicit final class VanillaAddressOps(val self: DexAddress) extends AnyVal {
     def toPB: PbByteString = self.bytes.toPB
   }
 
-  implicit final class VanillaOrderOps(val order: VOrder) extends AnyVal {
+  implicit final class VanillaOrderOps(val order: DexOrder) extends AnyVal {
 
     def toPB: PbOrder =
       PbOrder(
-        chainId = VAddressScheme.current.chainId.toInt,
+        chainId = DexAddressScheme.current.chainId.toInt,
         sender = order.orderAuthentication match {
-          case OrderAuthentication.OrderProofs(sender, _) => PbOrder.Sender.SenderPublicKey(sender.toPB)
-          case OrderAuthentication.Eip712Signature(sig) => PbOrder.Sender.Eip712Signature(sig.toPB)
+          case DexOrderAuthentication.OrderProofs(sender, _) => PbOrder.Sender.SenderPublicKey(sender.toPB)
+          case DexOrderAuthentication.Eip712Signature(sig) => PbOrder.Sender.Eip712Signature(sig.toPB)
         },
         matcherPublicKey = order.matcherPublicKey.toPB,
         assetPair = Some(PbAssetPair(order.assetPair.amountAsset.toPB, order.assetPair.priceAsset.toPB)),
         orderSide = order.orderType match {
-          case VOrderType.BUY => PbOrder.Side.BUY
-          case VOrderType.SELL => PbOrder.Side.SELL
+          case DexOrderType.BUY => PbOrder.Side.BUY
+          case DexOrderType.SELL => PbOrder.Side.SELL
         },
         amount = order.amount,
         price = order.price,
@@ -95,7 +95,7 @@ object DexToPbConversions {
 
   }
 
-  implicit final class VanillaByteStrOps(val self: VByteStr) extends AnyVal {
+  implicit final class VanillaByteStrOps(val self: DexByteStr) extends AnyVal {
     def toPB: PbByteString = UnsafeByteOperations.unsafeWrap(self) // PbByteString.copyFrom(self.arr)
   }
 

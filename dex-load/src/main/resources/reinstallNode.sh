14d6b7868c0dc5ffb9d1846a99e114312d8007ea
nik <ngadiyak@gmail.com>
2022-08-11 15:36:37 +0300

    DEX-1672

--- dex-load/src/main/resources/reinstallNode.sh
+++ dex-load/src/main/resources/reinstallNode.sh
@@ -5,4 +5,4 @@ dpkg -i /home/buildagent-matcher/waves-devnet*.deb
 systemctl start waves-devnet
 rm -rf /home/buildagent-matcher/*
 iptables -I INPUT -p tcp -m tcp --dport 443 -j ACCEPT
-iptables -I INPUT -p tcp -m tcp --dport 6867 -j ACCEPT
+iptables -I INPUT -p tcp -m tcp --dport 6887 -j ACCEPT

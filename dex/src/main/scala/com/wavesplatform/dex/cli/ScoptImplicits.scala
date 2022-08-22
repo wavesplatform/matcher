c335b07ccb5d79fd84511292c8a5356f23d91119
kpavkin <kpavkin@web3tech.ru>
2022-08-22 17:07:32 +0300

    DEX-1679 normalize variable name

--- dex/src/main/scala/com/wavesplatform/dex/cli/ScoptImplicits.scala
+++ dex/src/main/scala/com/wavesplatform/dex/cli/ScoptImplicits.scala
@@ -11,7 +11,7 @@ trait ScoptImplicits {
     }
   }
 
-  implicit val byteScoptREad: scopt.Read[Byte] = scopt.Read.reads { x =>
+  implicit val byteScoptRead: scopt.Read[Byte] = scopt.Read.reads { x =>
     x.toByteOption match {
       case Some(x: Byte) => x
       case _ => throw new IllegalArgumentException("'" + x + "' is not a byte.")

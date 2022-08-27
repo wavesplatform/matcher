5f5f3da132360c1dc05f7c4ee52225ad91be6e8f
Alexander Branevskiy <abranevskiy@web3tech.ru>
2022-08-27 20:13:06 +0300

    DEX-1609 impl

--- dex/src/main/scala/com/wavesplatform/dex/db/OrderDb.scala
+++ dex/src/main/scala/com/wavesplatform/dex/db/OrderDb.scala
@@ -1,14 +1,16 @@
 package com.wavesplatform.dex.db
 
-import cats.Functor
-import cats.syntax.functor._
-import com.wavesplatform.dex.db.leveldb.LevelDb
+import com.wavesplatform.dex.db.leveldb._
 import com.wavesplatform.dex.domain.account.Address
 import com.wavesplatform.dex.domain.asset.AssetPair
 import com.wavesplatform.dex.domain.bytes.ByteStr
 import com.wavesplatform.dex.domain.order.Order
 import com.wavesplatform.dex.model.OrderInfo.FinalOrderInfo
 import com.wavesplatform.dex.model.{OrderInfo, OrderStatus}
+import org.iq80.leveldb.DB
+
+import java.util.concurrent.Executors
+import scala.concurrent.{ExecutionContext, ExecutionContextExecutorService, Future}
 
 /**
  * Contains only finalized orders
@@ -27,67 +29,90 @@ trait OrderDb[F[_]] {
 object OrderDb {
   case class Settings(maxOrders: Int)
 
-  def levelDb[F[_]: Functor](settings: Settings, levelDb: LevelDb[F]): OrderDb[F] = new OrderDb[F] {
+  def levelDb(settings: Settings, db: DB): OrderDb[Future] =
+    levelDb(settings, db, Map(0 -> ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(1))))
 
-    override def containsInfo(id: Order.Id): F[Boolean] =
-      levelDb.readOnly(_.has(DbKeys.orderInfo(id)))
+  def levelDb(settings: Settings, db: DB, levelDbEcMap: Map[Int, ExecutionContextExecutorService]): OrderDb[Future] = new OrderDb[Future] {
 
-    override def status(id: Order.Id): F[OrderStatus.Final] = levelDb.readOnly { ro =>
-      ro.get(DbKeys.orderInfo(id)).fold[OrderStatus.Final](OrderStatus.NotFound)(_.status)
-    }
+    override def containsInfo(id: Order.Id): Future[Boolean] = Future {
+      db.readOnly(_.has(DbKeys.orderInfo(id)))
+    }(getEcByOrderId(id))
 
-    override def saveOrder(o: Order): F[Unit] = levelDb.readWrite { rw =>
-      val k = DbKeys.order(o.id())
-      if (!rw.has(k))
-        rw.put(k, Some(o))
-    }
+    override def status(id: Order.Id): Future[OrderStatus.Final] = Future {
+      db.readOnly(ro => ro.get(DbKeys.orderInfo(id)).fold[OrderStatus.Final](OrderStatus.NotFound)(_.status))
+    }(getEcByOrderId(id))
 
-    override def get(id: Order.Id): F[Option[Order]] = levelDb.readOnly(_.get(DbKeys.order(id)))
+    override def saveOrder(o: Order): Future[Unit] = Future {
+      db.readWrite { rw =>
+        val k = DbKeys.order(o.id())
+        if (!rw.has(k))
+          rw.put(k, Some(o))
+      }
+    }(getEcByOrderId(o.id()))
 
-    override def saveOrderInfo(id: Order.Id, oi: FinalOrderInfo): F[Unit] = {
+    override def get(id: Order.Id): Future[Option[Order]] = Future {
+      db.readOnly(_.get(DbKeys.order(id)))
+    }(getEcByOrderId(id))
+
+    override def saveOrderInfo(id: Order.Id, oi: FinalOrderInfo): Future[Unit] = {
       val orderInfoKey = DbKeys.orderInfo(id)
-      levelDb.readWrite { rw =>
-        if (!rw.has(orderInfoKey))
-          rw.put(orderInfoKey, Some(oi))
-      }
-    }
-
-    override def saveOrderInfoForHistory(id: Order.Id, sender: Address, oi: OrderInfo[OrderStatus.Final]): F[Unit] = {
-      val orderInfoKey = DbKeys.orderInfoForHistory(id)
-      levelDb.readWrite { rw =>
-        if (!rw.has(orderInfoKey)) {
-          val newCommonSeqNr = rw.inc(DbKeys.finalizedCommonSeqNr(sender))
-          rw.put(DbKeys.finalizedCommon(sender, newCommonSeqNr), Some(id))
-
-          val newPairSeqNr = rw.inc(DbKeys.finalizedPairSeqNr(sender, oi.assetPair))
-          rw.put(DbKeys.finalizedPair(sender, oi.assetPair, newPairSeqNr), Some(id))
-          if (newPairSeqNr > settings.maxOrders) // Indexes start with 1, so if maxOrders=100 and newPairSeqNr=101, we delete 1 (the first)
-            rw.get(DbKeys.finalizedPair(sender, oi.assetPair, newPairSeqNr - settings.maxOrders))
-              .map(DbKeys.order)
-              .foreach(x => rw.delete(x))
+      Future {
+        db.readWrite { rw =>
+          if (!rw.has(orderInfoKey))
+            rw.put(orderInfoKey, Some(oi))
         }
-      }
+      }(getEcByOrderId(id))
     }
 
-    override def getFinalizedOrders(owner: Address, maybePair: Option[AssetPair]): F[Seq[(Order.Id, OrderInfo[OrderStatus])]] =
-      levelDb.readOnly { ro =>
-        val (seqNr, key) =
-          maybePair match {
-            case Some(p) =>
-              (ro.get(DbKeys.finalizedPairSeqNr(owner, p)), DbKeys.finalizedPair(owner, p, _: Int))
-            case None =>
-              (ro.get(DbKeys.finalizedCommonSeqNr(owner)), DbKeys.finalizedCommon(owner, _: Int))
+    override def saveOrderInfoForHistory(id: Order.Id, sender: Address, oi: OrderInfo[OrderStatus.Final]): Future[Unit] = {
+      val orderInfoKey = DbKeys.orderInfoForHistory(id)
+      Future {
+        db.readWrite { rw =>
+          if (!rw.has(orderInfoKey)) {
+            val newCommonSeqNr = rw.inc(DbKeys.finalizedCommonSeqNr(sender))
+            rw.put(DbKeys.finalizedCommon(sender, newCommonSeqNr), Some(id))
+
+            val newPairSeqNr = rw.inc(DbKeys.finalizedPairSeqNr(sender, oi.assetPair))
+            rw.put(DbKeys.finalizedPair(sender, oi.assetPair, newPairSeqNr), Some(id))
+            if (newPairSeqNr > settings.maxOrders) // Indexes start with 1, so if maxOrders=100 and newPairSeqNr=101, we delete 1 (the first)
+              rw.get(DbKeys.finalizedPair(sender, oi.assetPair, newPairSeqNr - settings.maxOrders))
+                .map(DbKeys.order)
+                .foreach(x => rw.delete(x))
           }
+        }
+      }(getEcBySender(sender))
+    }
 
-        for {
-          offset <- 0 until math.min(seqNr, settings.maxOrders)
-          id <- ro.get(key(seqNr - offset))
-          oi <- ro.get(DbKeys.orderInfo(id))
-        } yield id -> oi
-      }.map(_.sorted(orderInfoOrdering))
+    override def getFinalizedOrders(owner: Address, maybePair: Option[AssetPair]): Future[Seq[(Order.Id, OrderInfo[OrderStatus])]] = {
+      val ec = getEcBySender(owner)
+      Future {
+        db.readOnly { ro =>
+          val (seqNr, key) =
+            maybePair match {
+              case Some(p) =>
+                (ro.get(DbKeys.finalizedPairSeqNr(owner, p)), DbKeys.finalizedPair(owner, p, _: Int))
+              case None =>
+                (ro.get(DbKeys.finalizedCommonSeqNr(owner)), DbKeys.finalizedCommon(owner, _: Int))
+            }
 
-    override def getOrderInfo(id: Order.Id): F[Option[FinalOrderInfo]] =
-      levelDb.readOnly(_.get(DbKeys.orderInfo(id)))
+          for {
+            offset <- 0 until math.min(seqNr, settings.maxOrders)
+            id <- ro.get(key(seqNr - offset))
+            oi <- ro.get(DbKeys.orderInfo(id))
+          } yield id -> oi
+        }
+      }(ec).map(_.sorted(orderInfoOrdering))(ec)
+    }
+
+    override def getOrderInfo(id: Order.Id): Future[Option[FinalOrderInfo]] = Future {
+      db.readOnly(_.get(DbKeys.orderInfo(id)))
+    }(getEcByOrderId(id))
+
+    private def getEcByOrderId(id: Order.Id): ExecutionContextExecutorService =
+      levelDbEcMap(id.base58.hashCode % levelDbEcMap.size)
+
+    private def getEcBySender(sender: Address): ExecutionContextExecutorService =
+      levelDbEcMap(sender.stringRepr.hashCode % levelDbEcMap.size)
 
   }
 

e1ca852944bca1c6e0bb89859351463624be49ad
alexander-branevskiy <alexander-branevskiy@gmail.com>
2022-09-10 16:36:18 +0700

    DEX-1669 wip

--- dex/src/main/scala/com/wavesplatform/dex/db/OrderDb.scala
+++ dex/src/main/scala/com/wavesplatform/dex/db/OrderDb.scala
@@ -30,90 +30,99 @@ object OrderDb {
   case class Settings(maxOrders: Int)
 
   def levelDb(settings: Settings, db: DB): OrderDb[Future] =
-    levelDb(settings, db, Map(0 -> ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(1))))
+    levelDb(settings, db, Map(0 -> ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(1))), useVar = false)
 
-  def levelDb(settings: Settings, db: DB, levelDbEcMap: Map[Int, ExecutionContextExecutorService]): OrderDb[Future] = new OrderDb[Future] {
+  def levelDb(settings: Settings, db: DB, levelDbEcMap: Map[Int, ExecutionContextExecutorService], useVar: Boolean): OrderDb[Future] =
+    new OrderDb[Future] {
 
-    override def containsInfo(id: Order.Id): Future[Boolean] = Future {
-      db.readOnly(_.has(DbKeys.orderInfo(id)))
-    }(getEcByOrderId(id))
+      private val ec = ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(1))
 
-    override def status(id: Order.Id): Future[OrderStatus.Final] = Future {
-      db.readOnly(ro => ro.get(DbKeys.orderInfo(id)).fold[OrderStatus.Final](OrderStatus.NotFound)(_.status))
-    }(getEcByOrderId(id))
+      override def containsInfo(id: Order.Id): Future[Boolean] = Future {
+        db.readOnly(_.has(DbKeys.orderInfo(id)))
+      }(getEcByOrderId(id))
 
-    override def saveOrder(o: Order): Future[Unit] = Future {
-      db.readWrite { rw =>
-        val k = DbKeys.order(o.id())
-        if (!rw.has(k))
-          rw.put(k, Some(o))
-      }
-    }(getEcByOrderId(o.id()))
+      override def status(id: Order.Id): Future[OrderStatus.Final] = Future {
+        db.readOnly(ro => ro.get(DbKeys.orderInfo(id)).fold[OrderStatus.Final](OrderStatus.NotFound)(_.status))
+      }(getEcByOrderId(id))
 
-    override def get(id: Order.Id): Future[Option[Order]] = Future {
-      db.readOnly(_.get(DbKeys.order(id)))
-    }(getEcByOrderId(id))
-
-    override def saveOrderInfo(id: Order.Id, oi: FinalOrderInfo): Future[Unit] = Future {
-      val orderInfoKey = DbKeys.orderInfo(id)
-      db.readWrite { rw =>
-        if (!rw.has(orderInfoKey))
-          rw.put(orderInfoKey, Some(oi))
-      }
-    }(getEcByOrderId(id))
-
-    override def saveOrderInfoForHistory(id: Order.Id, sender: Address, oi: OrderInfo[OrderStatus.Final]): Future[Unit] = Future {
-      val orderInfoKey = DbKeys.orderInfoForHistory(sender, id)
-      db.readWrite { rw =>
-        if (!rw.has(orderInfoKey)) {
-          val newCommonSeqNr = rw.inc(DbKeys.finalizedCommonSeqNr(sender))
-          rw.put(DbKeys.finalizedCommon(sender, newCommonSeqNr), Some(id))
-
-          val newPairSeqNr = rw.inc(DbKeys.finalizedPairSeqNr(sender, oi.assetPair))
-          rw.put(DbKeys.finalizedPair(sender, oi.assetPair, newPairSeqNr), Some(id))
-          if (newPairSeqNr > settings.maxOrders) // Indexes start with 1, so if maxOrders=100 and newPairSeqNr=101, we delete 1 (the first)
-            rw.get(DbKeys.finalizedPair(sender, oi.assetPair, newPairSeqNr - settings.maxOrders))
-              .map(DbKeys.order)
-              .foreach(x => rw.delete(x))
-          rw.put(orderInfoKey, Some(oi))
+      override def saveOrder(o: Order): Future[Unit] = Future {
+        db.readWrite { rw =>
+          val k = DbKeys.order(o.id())
+          if (!rw.has(k))
+            rw.put(k, Some(o))
         }
-      }
-    }(getEcBySender(sender))
+      }(getEcByOrderId(o.id()))
 
-    override def getFinalizedOrders(owner: Address, maybePair: Option[AssetPair]): Future[Seq[(Order.Id, OrderInfo[OrderStatus])]] = {
-      val ec = getEcBySender(owner)
-      Future {
-        db.readOnly { ro =>
-          val (seqNr, key) =
-            maybePair match {
-              case Some(p) =>
-                (ro.get(DbKeys.finalizedPairSeqNr(owner, p)), DbKeys.finalizedPair(owner, p, _: Int))
-              case None =>
-                (ro.get(DbKeys.finalizedCommonSeqNr(owner)), DbKeys.finalizedCommon(owner, _: Int))
-            }
+      override def get(id: Order.Id): Future[Option[Order]] = Future {
+        db.readOnly(_.get(DbKeys.order(id)))
+      }(getEcByOrderId(id))
 
-          for {
-            offset <- 0 until math.min(seqNr, settings.maxOrders)
-            id <- ro.get(key(seqNr - offset))
-            oi <- ro.get(DbKeys.orderInfoForHistory(owner, id))
-          } yield id -> oi
+      override def saveOrderInfo(id: Order.Id, oi: FinalOrderInfo): Future[Unit] = Future {
+        val orderInfoKey = DbKeys.orderInfo(id)
+        db.readWrite { rw =>
+          if (!rw.has(orderInfoKey))
+            rw.put(orderInfoKey, Some(oi))
         }
-      }(ec).map(_.sorted(orderInfoOrdering))(ec)
+      }(getEcByOrderId(id))
+
+      override def saveOrderInfoForHistory(id: Order.Id, sender: Address, oi: OrderInfo[OrderStatus.Final]): Future[Unit] = Future {
+        val orderInfoKey = DbKeys.orderInfoForHistory(sender, id)
+        db.readWrite { rw =>
+          if (!rw.has(orderInfoKey)) {
+            val newCommonSeqNr = rw.inc(DbKeys.finalizedCommonSeqNr(sender))
+            rw.put(DbKeys.finalizedCommon(sender, newCommonSeqNr), Some(id))
+
+            val newPairSeqNr = rw.inc(DbKeys.finalizedPairSeqNr(sender, oi.assetPair))
+            rw.put(DbKeys.finalizedPair(sender, oi.assetPair, newPairSeqNr), Some(id))
+            if (newPairSeqNr > settings.maxOrders) // Indexes start with 1, so if maxOrders=100 and newPairSeqNr=101, we delete 1 (the first)
+              rw.get(DbKeys.finalizedPair(sender, oi.assetPair, newPairSeqNr - settings.maxOrders))
+                .map(DbKeys.order)
+                .foreach(x => rw.delete(x))
+            rw.put(orderInfoKey, Some(oi))
+          }
+        }
+      }(getEcBySender(sender))
+
+      override def getFinalizedOrders(owner: Address, maybePair: Option[AssetPair]): Future[Seq[(Order.Id, OrderInfo[OrderStatus])]] = {
+        val ec = getEcBySender(owner)
+        Future {
+          db.readOnly { ro =>
+            val (seqNr, key) =
+              maybePair match {
+                case Some(p) =>
+                  (ro.get(DbKeys.finalizedPairSeqNr(owner, p)), DbKeys.finalizedPair(owner, p, _: Int))
+                case None =>
+                  (ro.get(DbKeys.finalizedCommonSeqNr(owner)), DbKeys.finalizedCommon(owner, _: Int))
+              }
+
+            for {
+              offset <- 0 until math.min(seqNr, settings.maxOrders)
+              id <- ro.get(key(seqNr - offset))
+              oi <- ro.get(DbKeys.orderInfoForHistory(owner, id))
+            } yield id -> oi
+          }
+        }(ec).map(_.sorted(orderInfoOrdering))(ec)
+      }
+
+      override def getOrderInfo(id: Order.Id): Future[Option[FinalOrderInfo]] = Future {
+        db.readOnly(_.get(DbKeys.orderInfo(id)))
+      }(getEcByOrderId(id))
+
+      //0x7FFFFFFF "hack" is taken from java.util.Hashtable in order to deal with negative hash codes
+
+      private def getEcByOrderId(id: Order.Id): ExecutionContextExecutorService =
+        if (useVar)
+          ec
+        else
+          levelDbEcMap((id.base58.hashCode & 0x7fffffff) % levelDbEcMap.size)
+
+      private def getEcBySender(sender: Address): ExecutionContextExecutorService =
+        if (useVar)
+          ec
+        else
+          levelDbEcMap((sender.stringRepr.hashCode & 0x7fffffff) % levelDbEcMap.size)
+
     }
-
-    override def getOrderInfo(id: Order.Id): Future[Option[FinalOrderInfo]] = Future {
-      db.readOnly(_.get(DbKeys.orderInfo(id)))
-    }(getEcByOrderId(id))
-
-    //0x7FFFFFFF "hack" is taken from java.util.Hashtable in order to deal with negative hash codes
-
-    private def getEcByOrderId(id: Order.Id): ExecutionContextExecutorService =
-      levelDbEcMap((id.base58.hashCode & 0x7fffffff) % levelDbEcMap.size)
-
-    private def getEcBySender(sender: Address): ExecutionContextExecutorService =
-      levelDbEcMap((sender.stringRepr.hashCode & 0x7fffffff) % levelDbEcMap.size)
-
-  }
 
   val orderIdOrdering: Ordering[(Order.Id, Long)] = Ordering.by { case (id, ts) => (-ts, id) }
 
